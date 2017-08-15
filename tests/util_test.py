ce18d279a12652de5ed8a4b9b099bbc31f1b1315
Kim Jin Su <item4_hun@hotmail.com>
2017-08-15 18:53:11 +0900

    Make helper functions for slack syntax

--- tests/util_test.py
+++ tests/util_test.py
@@ -1,4 +1,4 @@
-from yui.util import bool2str
+from yui.util import bold, bool2str, code, italics, preformatted, quote, strike
 
 
 def test_bool2str():
@@ -6,3 +6,14 @@ def test_bool2str():
 
     assert bool2str(True) == 'true'
     assert bool2str(False) == 'false'
+
+
+def text_slack_syntax():
+    """Test slack syntax helpers."""
+
+    assert bold('item4') == '*item4*'
+    assert code('item4') == '`item4`'
+    assert italics('item4') == '_item4_'
+    assert preformatted('item4') == '```item4```'
+    assert strike('item4') == '~item4~'
+    assert quote('item4') == '>item4'
--- yui/util.py
+++ yui/util.py
@@ -1,4 +1,48 @@
-__all__ = 'bool2str'
+__all__ = (
+    'bold',
+    'bool2str',
+    'code',
+    'italics',
+    'preformatted',
+    'quote',
+    'strike',
+)
+
+
+def bold(text: str) -> str:
+    """Make text to bold."""
+
+    return f'*{text}*'
+
+
+def italics(text: str) -> str:
+    """Make text to italics."""
+
+    return f'_{text}_'
+
+
+def strike(text: str) -> str:
+    """Make text to strike."""
+
+    return f'~{text}~'
+
+
+def code(text: str) -> str:
+    """Make text to code."""
+
+    return f'`{text}`'
+
+
+def preformatted(text: str) -> str:
+    """Make text to pre-formatted text."""
+
+    return f'```{text}```'
+
+
+def quote(text: str) -> str:
+    """Make text to qoute."""
+
+    return f'>{text}'
 
 
 def bool2str(value: bool) -> str:
