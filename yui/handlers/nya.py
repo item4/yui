973ee87ea77d64756a1eebfdd5ba0e9075433ae8
kjwon15 <kjwonmail@gmail.com>
2017-11-16 01:15:32 +0900

    Implement nyapic

---
+++ yui/handlers/nya.py
@@ -0,0 +1,43 @@
+import aiohttp
+import datetime
+import json
+
+from ..box import box
+from ..event import Message
+
+
+def static_vars(**kwargs):
+    def decorator(func):
+        for key, val in kwargs.items():
+            setattr(func, key, val)
+
+        return func
+    return decorator
+
+
+@box.command('nya')
+@static_vars({'last_call': None})
+async def nya(bot, event: Message, sess):
+    cool_time = datetime.timedelta(minutes=10)
+    now = datetime.datetime.utcnow()
+    if nya.last_call is not None and now - nya.last_call < cool_time:
+        bot.say(
+            event.channel,
+            '아직 쿨타임이다냥'
+        )
+        return
+
+    nya.last_call = now
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://api.giphy.com/v1/gifs/random', params={
+            'api_key': bot.config.GIPHY_API_KEY,
+            'tag': 'cat',
+        }) as resp:
+            json_result = json.loads(await resp.read())
+            url = json_result['data']['image_url']
+
+            await bot.say(
+                event.channel,
+                url
+            )
