52d7ec0f4953ed4a6cf6992de2bde8277b8eb34f
Kim Jin Su <item4_hun@hotmail.com>
2017-11-16 00:12:59 +0900

    Bump pytest

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ install_requires = {
 
 tests_require = {
     'mypy == 0.550',
-    'pytest == 3.2.4',
+    'pytest == 3.2.5',
     'pytest-asyncio == 0.8.0',
     'pytest-cov == 2.5.1',
     'mocket == 2.1.1',
