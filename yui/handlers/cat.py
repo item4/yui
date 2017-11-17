7f6f7d75ea94e3af374526e3dc51dff4ec477fdd
kjwon15 <kjwonmail@gmail.com>
2017-11-17 23:20:58 +0900

    Add docstring

--- yui/handlers/cat.py
+++ yui/handlers/cat.py
@@ -10,6 +10,13 @@ from ..util import static_vars
 @box.command('cat')
 @static_vars(last_call=None)
 async def cat(bot, event: Message, sess):
+    """
+    냥냥이 짤을 수급합니다.
+    쿨타임은 10분입니다.
+
+    `{PREFIX}cat`: 냐짤 수급
+    """
+
     cool_time = datetime.timedelta(minutes=10)
     now = datetime.datetime.utcnow()
     if cat.last_call is not None and now - cat.last_call < cool_time:
