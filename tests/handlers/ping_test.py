35b8492e134af90d0f9305d92efdfa3de30520c5
Kim Jin Su <item4_hun@hotmail.com>
2017-11-21 16:18:47 +0900

    Test ping command

---
+++ tests/handlers/ping_test.py
@@ -0,0 +1,26 @@
+import pytest
+
+from yui.event import create_event
+from yui.handlers.ping import ping
+
+from ..util import FakeBot
+
+
+@pytest.mark.asyncio
+async def test_ping_command():
+    bot = FakeBot()
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+    })
+    user = {
+        'user': {
+            'name': 'item4',
+        },
+    }
+
+    await ping(bot, event, user)
+    said = bot.call_queue.pop()
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '@item4, pong!'
