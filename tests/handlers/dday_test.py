44d15289553bc253a9a0f6831eb21ad2722e19bc
Kim Jin Su <item4_hun@hotmail.com>
2017-11-21 18:53:34 +0900

    Test dday command

---
+++ tests/handlers/dday_test.py
@@ -0,0 +1,35 @@
+import datetime
+
+import pytest
+
+from yui.event import create_event
+from yui.handlers.dday import dday
+
+from ..util import FakeBot
+
+
+@pytest.mark.asyncio
+async def test_age_command():
+    bot = FakeBot()
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+    })
+    jan = datetime.date(2000, 1, 1)
+    feb = datetime.date(2000, 2, 1)
+
+    await dday(bot, event, jan, feb)
+    said = bot.call_queue.pop()
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '2000년 01월 01일로부터 2000년 02월 01일까지 31일 남았어요!'
+    )
+
+    await dday(bot, event, feb, jan)
+    said = bot.call_queue.pop()
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '2000년 01월 01일로부터 2000년 02월 01일까지 31일 지났어요!'
+    )
