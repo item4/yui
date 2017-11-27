7c66ac6b78cd7d0e4d892490a0b58a7efc1fea7a
Kim Jin Su <item4_hun@hotmail.com>
2017-11-27 20:17:13 +0900

    Test hi handler

---
+++ tests/handlers/hi_test.py
@@ -0,0 +1,80 @@
+import pytest
+
+from yui.event import create_event
+from yui.handlers.hi import hi
+
+from ..util import FakeBot
+
+
+@pytest.mark.asyncio
+async def test_age_command():
+    bot = FakeBot()
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+        'user': 'U1',
+        'text': '안녕 유이',
+    })
+
+    @bot.response('users.info')
+    def users_info(data):
+        return {
+            'user': {
+                'name': 'kirito',
+            },
+        }
+
+    await hi(bot, event)
+
+    user_query = bot.call_queue.pop(0)
+    assert user_query.method == 'users.info'
+    assert user_query.data['user'] == 'U1'
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '안녕하세요! @kirito'
+
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+        'user': 'U1',
+        'text': '안녕',
+    })
+
+    await hi(bot, event)
+
+    user_query = bot.call_queue.pop(0)
+    assert user_query.method == 'users.info'
+    assert user_query.data['user'] == 'U1'
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '안녕하세요! @kirito'
+
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+        'user': 'U1',
+        'text': '유이 안녕',
+    })
+
+    await hi(bot, event)
+
+    user_query = bot.call_queue.pop(0)
+    assert user_query.method == 'users.info'
+    assert user_query.data['user'] == 'U1'
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '안녕하세요! @kirito'
+
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+        'user': 'U1',
+        'text': '아무말 대잔치',
+    })
+
+    await hi(bot, event)
+
+    assert not bot.call_queue
--- tests/util.py
+++ tests/util.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, NamedTuple
+from typing import Any, Callable, Dict, List, NamedTuple
 
 from yui.api import SlackAPI
 from yui.bot import Bot
@@ -18,9 +18,19 @@ class FakeBot(Bot):
         self.call_queue: List[Call] = []
         self.api = SlackAPI(self)
         self.channels: Dict[str, Dict[str, Any]] = {}
+        self.responses: Dict[str, Callable] = {}
 
     async def call(self, method: str, data: Dict[str, str]=None):
         self.call_queue.append(Call(method, data))
+        callback = self.responses.get(method)
+        if callback:
+            return callback(data)
+
+    def response(self, method: str):
+        def decorator(func):
+            self.responses[method] = func
+            return func
+        return decorator
 
 
 class FakeImportLib:
