b0e32388dea7dc00b662132d6b70864e8f1a1698
Kim Jin Su <item4_hun@hotmail.com>
2018-06-09 02:03:31 +0900

    Add new closers overflood dungeon times

--- yui/handlers/closers/crontab.py
+++ yui/handlers/closers/crontab.py
@@ -1,7 +1,7 @@
 from ...box import box
 
 
-@box.crontab('30 9,14,19,22 * * 0,1,5')
+@box.crontab('30 9,14,17,19,22 * * 0,1,5')
 async def overflood_front_before_30m(bot):
     await bot.say(
         '#game',
@@ -9,7 +9,7 @@ async def overflood_front_before_30m(bot):
     )
 
 
-@box.crontab('0 10,15,20,23 * * 0,1,5')
+@box.crontab('0 10,15,18,20,23 * * 0,1,5')
 async def overflood_front_start(bot):
     await bot.say(
         '#game',
@@ -17,7 +17,7 @@ async def overflood_front_start(bot):
     )
 
 
-@box.crontab('30 9,14,19,22 * * 2,4,6')
+@box.crontab('30 9,14,17,19,22 * * 2,4,6')
 async def overflood_back_before_30m(bot):
     await bot.say(
         '#game',
@@ -25,8 +25,40 @@ async def overflood_back_before_30m(bot):
     )
 
 
-@box.crontab('0 10,15,20,23 * * 2,4,6')
+@box.crontab('0 10,15,18,20,23 * * 2,4,6')
 async def overflood_back_start(bot):
+    await bot.say(
+        '#game',
+        ':closers: 오염지옥 오버플루드(이면) 개장 시간입니다.'
+    )
+
+
+@box.crontab('0 0,1 * * 1,2,6')
+async def midnight_overflood_front_before_30m(bot):
+    await bot.say(
+        '#game',
+        ':closers: 오염지옥 오버플루드(표면) 개장 30분 전입니다.'
+    )
+
+
+@box.crontab('30 0,1 * * 1,2,6')
+async def midnight_overflood_front_start(bot):
+    await bot.say(
+        '#game',
+        ':closers: 오염지옥 오버플루드(표면) 개장 시간입니다.'
+    )
+
+
+@box.crontab('0 0,1 * * 0,3,5')
+async def midnight_overflood_back_before_30m(bot):
+    await bot.say(
+        '#game',
+        ':closers: 오염지옥 오버플루드(이면) 개장 30분 전입니다.'
+    )
+
+
+@box.crontab('30 0,1 * * 0,3,5')
+async def midnight_overflood_back_start(bot):
     await bot.say(
         '#game',
         ':closers: 오염지옥 오버플루드(이면) 개장 시간입니다.'
