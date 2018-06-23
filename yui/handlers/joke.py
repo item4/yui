477c6315184952cd24087341ca3159200acb5123
Jack Valentine <jackvalentine@jackarchive.com>
2018-06-23 20:26:32 +1000

    Long url change
    - relax text url change to short url.

--- yui/handlers/joke.py
+++ yui/handlers/joke.py
@@ -10,7 +10,7 @@ HASSAN_TRIGGER_RE = re.compile('^똑바로\s*서라\s*[,\.!]*\s*유이')
 async def relax(bot, event: Message):
     await bot.api.chat.postMessage(
         channel=event.channel,
-        text='https://pbs.twimg.com/profile_images/756486934747242496/Y0VaYlyr.jpg\r\n안심하십시오 괜찮습니다.',
+        text='https://goo.gl/cRjLEJ\r\n안심하십시오 괜찮습니다.',
         as_user=False,
         icon_url='https://i.imgur.com/GIHYPOx.jpg',
         username='우주안심수호자',
