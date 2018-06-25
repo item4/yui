87e75c4a8d3857a32c55aec15f7979ebf7f37989
Kim Jin Su <item4_hun@hotmail.com>
2018-06-25 13:05:56 +0900

    Fix core handler to handle APICallError

--- yui/handlers/core.py
+++ yui/handlers/core.py
@@ -1,6 +1,7 @@
+import asyncio
 import logging
 
-from ..bot import BotReconnect
+from ..bot import APICallError, BotReconnect
 from ..box import box
 from ..event import (
     ChannelArchive,
@@ -41,32 +42,60 @@ from ..type import (
 logger = logging.getLogger(__name__)
 
 
+async def retry(callback, *args, **kwargs):
+    while True:
+        try:
+            return await callback(*args, **kwargs)
+        except APICallError:
+            await asyncio.sleep(0.5)
+        except Exception:
+            raise
+
+
 @box.on(ChatterboxSystemStart)
 async def on_start(bot):
-    cursor = None
-    while True:
-        result = await bot.api.channels.list(cursor)
+    async def channel():
         cursor = None
-        if 'response_metadata' in result:
-            cursor = result['response_metadata'].get('next_cursor')
-        for c in result['channels']:
-            res = await bot.api.channels.info(c['id'])
-            bot.channels.append(PublicChannel(**res['channel']))
-        if not cursor:
-            break
+        bot.channels.clear()
+        while True:
+            result = await retry(bot.api.channels.list, cursor)
+            cursor = None
+            if 'response_metadata' in result:
+                cursor = result['response_metadata'].get('next_cursor')
+            for c in result['channels']:
+                res = await retry(bot.api.channels.info, c['id'])
+                bot.channels.append(PublicChannel(**res['channel']))
+            if not cursor:
+                break
 
-    result = await bot.api.im.list()
-    for d in result['ims']:
-        bot.ims.append(DirectMessageChannel(**d))
+    async def im():
+        bot.ims.clear()
+        result = await retry(bot.api.im.list)
+        for d in result['ims']:
+            bot.ims.append(DirectMessageChannel(**d))
 
-    result = await bot.api.groups.list()
-    for g in result['groups']:
-        res = await bot.api.groups.info(g['id'])
-        bot.groups.append(PrivateChannel(**res['group']))
+    async def groups():
+        bot.groups.clear()
+        result = await retry(bot.api.groups.list)
+        for g in result['groups']:
+            res = await retry(bot.api.groups.info, g['id'])
+            bot.groups.append(PrivateChannel(**res['group']))
 
-    result = await bot.api.users.list(presence=False)
-    for u in result['members']:
-        bot.users[u['id']] = User(**u)
+    async def users():
+        bot.users.clear()
+        result = await retry(bot.api.users.list, presence=False)
+        for u in result['members']:
+            bot.users[u['id']] = User(**u)
+
+    await asyncio.wait(
+        (
+            channel(),
+            im(),
+            groups(),
+            users(),
+        ),
+        return_when=asyncio.FIRST_EXCEPTION,
+    )
 
     return True
 
@@ -74,7 +103,7 @@ async def on_start(bot):
 @box.on(TeamJoin)
 async def on_team_join(bot, event: TeamJoin):
     logger.info('on team join start')
-    res = await bot.api.users.info(event.user)
+    res = await retry(bot.api.users.info, event.user)
     bot.users[res['user']['id']] = User(**res['user'])
     logger.info('on team join end')
 
@@ -84,7 +113,7 @@ async def on_team_join(bot, event: TeamJoin):
 @box.on(UserChange)
 async def on_user_change(bot, event: UserChange):
     logger.info('on user change start')
-    res = await bot.api.users.info(event.user)
+    res = await retry(bot.api.users.info, event.user)
     bot.users[res['user']['id']] = User(**res['user'])
     logger.info('on user change end')
 
@@ -105,10 +134,10 @@ async def public_channel_mutation_detected(bot):
     cursor = None
     new_channels = []
     while True:
-        result = await bot.api.channels.list(cursor)
+        result = await retry(bot.api.channels.list, cursor)
         cursor = result.get('response_metadata', {}).get('next_cursor')
         for c in result['channels']:
-            res = await bot.api.channels.info(c['id'])
+            res = await retry(bot.api.channels.info, c['id'])
             new_channels.append(PublicChannel(**res['channel']))
         if not cursor:
             break
@@ -130,9 +159,9 @@ async def public_channel_mutation_detected(bot):
 async def private_channel_mutation_detected(bot):
     logger.info('private_channel_mutation_detected start')
     new_groups = []
-    result = await bot.api.groups.list()
+    result = await retry(bot.api.groups.list)
     for g in result['groups']:
-        res = await bot.api.groups.info(g['id'])
+        res = await retry(bot.api.groups.info, g['id'])
         new_groups.append(PrivateChannel(**res['group']))
 
     bot.groups[:] = new_groups
@@ -148,7 +177,7 @@ async def private_channel_mutation_detected(bot):
 async def direct_message_channel_mutation_detected(bot):
     logger.info('direct_message_channel_mutation_detected start')
     new_ims = []
-    result = await bot.api.im.list()
+    result = await retry(bot.api.im.list)
     for d in result['ims']:
         new_ims.append(DirectMessageChannel(**d))
 
