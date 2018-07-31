102237878f15b638073f5a619aab49361ff68289
ignocide <ignocide@gmail.com>
2018-07-31 18:34:51 +0900

    remove function.partial syntax in gamble service

--- yui/handlers/gamble.py
+++ yui/handlers/gamble.py
@@ -4,27 +4,22 @@ import functools
 from ..box import box
 from ..event import Message
 
-
 @box.command('dice', ['주사위'])
 async def dice(bot, event: Message):
     number = random.randrange(0,100)
-    tens = number / 10
 
     if number == 2:
         text = '콩'
     elif number == 22: 
         text = '콩콩'
     else: 
-        text = random.choice(['힘껏 던진 결과입니다. {}입니다','{}! 하하하','{}','유이가 기도하며 주사위를 굴려줬습니다. {}입니다.','날아가던 주사위를 냥이가 쳐서 {}입니다.','소라고둥님이 {}이랍니다.'])
+        text = random.choice(['힘껏 던진 결과입니다. {}입니다','{}! 하하하','{}','유이가 기도하며 주사위를 굴려줬습니다. {}입니다.','날아가던 주사위를 냥이가 쳐버렸네요. {}입니다.','소라고둥님이 {}이랍니다.'])
         text = text.format(number)
 
-    dealer_say = functools.partial(
-        bot.api.chat.postMessage,
+    await bot.api.chat.postMessage(
         channel=event.channel,
         as_user=False,
         username='딜러',
         icon_url='https://i.imgur.com/8OcjS3o.jpg',
         text = text
     )
-
-    await dealer_say()
