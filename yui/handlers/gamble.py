85b3184f3ff5f1e8839a2cf9030ff7a3c16f4f15
ignocide <ignocide@gmail.com>
2018-08-03 18:37:55 +0900

    adapt code style rule, maximum line length of 79 characters and comma at end

--- yui/handlers/gamble.py
+++ yui/handlers/gamble.py
@@ -14,8 +14,10 @@ async def dice(bot, event: Message):
         text = '콩콩'
     else:
         text = random.choice([
-            '힘껏 던진 결과입니다. {}입니다', '{}! 하하하', '{}', '유이가 기도하며 주사위를 굴려줬습니다. {}입니다.',
-            '날아가던 주사위를 냥이가 쳐버렸네요. {}입니다.', '소라고둥님이 {}이랍니다.'
+            '힘껏 던진 결과입니다. {}입니다', '{}! 하하하', '{}',
+            '유이가 기도하며 주사위를 굴려줬습니다. {}입니다.',
+            '날아가던 주사위를 냥이가 쳐버렸네요. {}입니다.',
+            '소라고둥님이 {}이랍니다.',
         ])
         text = text.format(number)
 
@@ -24,5 +26,5 @@ async def dice(bot, event: Message):
         as_user=False,
         username='딜러',
         icon_url='https://i.imgur.com/8OcjS3o.jpg',
-        text=text
+        text=text,
     )
