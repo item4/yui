db856ae510bdf3678e3483244fd46d80e02bfe36
Kim Jin Su <item4_hun@hotmail.com>
2018-09-08 23:51:43 +0900

    Refactor animation sub search command
    1. Support search without anissia response
    2. Supoort search without anissia record
    3. Increase search ratio if two record has some same value
    4. Change finished sub search action to print all matching results
    5. Support well-detecting same sub url for remove dup

--- yui/handlers/sub.py
+++ yui/handlers/sub.py
@@ -1,7 +1,7 @@
 import asyncio
-import datetime
 import math
 import urllib.parse
+from datetime import datetime
 from typing import Any, Dict, List, NamedTuple
 
 import aiohttp
@@ -23,7 +23,7 @@ class Sub(NamedTuple):
     maker: str
     episode_num: float
     url: str
-    released_at: datetime.datetime
+    released_at: str
 
 
 DOW = [
@@ -43,6 +43,16 @@ def print_time(t: str) -> str:
     return '{}:{}'.format(t[:2], t[2:])
 
 
+def encode_url(u: str) -> str:
+    prefix = ''
+    if u.startswith('http://'):
+        prefix = 'http://'
+    elif u.startswith('https://'):
+        prefix = 'https://'
+    u = u[len(prefix):]
+    return prefix + '/'.join(urllib.parse.quote(c) for c in u.split('/'))
+
+
 def fix_url(url: str) -> str:
     if url.startswith('https://') or url.startswith('http://'):
         return url
@@ -54,22 +64,29 @@ def make_sub_list(data: List[Sub]) -> List[Attachment]:
 
     if data:
         for sub in data:
+            num = '완결' if sub.episode_num == 9999 else f'{sub.episode_num}화'
+            name = sub.maker
+            url = fix_url(sub.url)
+            released_at = None
+            try:
+                released_at = datetime.strptime(
+                    sub.released_at,
+                    '%Y%m%d%H%M%S',
+                )
+            except ValueError:
+                pass
+            if released_at:
+                date = released_at.strftime(DATE_FORMAT)
+                fallback = f'{num} {date} {name} {url}'
+                text = f'{num} {date} {url}'
+            else:
+                fallback = f'{num} {name} {url}'
+                text = f'{num} {url}'
             result.append(
                 Attachment(
-                    fallback='{} {} {} {}'.format(
-                        '완결' if sub.episode_num == 9999
-                        else f'{sub.episode_num}화',
-                        sub.released_at.strftime(DATE_FORMAT),
-                        sub.maker,
-                        fix_url(sub.url),
-                    ),
-                    author_name=sub.maker,
-                    text='{} {} {}'.format(
-                        '완결' if sub.episode_num == 9999
-                        else f'{sub.episode_num}화',
-                        sub.released_at.strftime(DATE_FORMAT),
-                        fix_url(sub.url),
-                    ),
+                    fallback=fallback,
+                    author_name=name,
+                    text=text,
                 )
             )
     else:
@@ -128,12 +145,13 @@ async def sub(bot, event: Message, finished: bool, title: str):
     ALIAS를 기준으로 삼아 애니시아의 자막 목록에서 검색합니다. 따라서 OHLI와 애니시아의 애니명이
     다른 경우에는 검색이 가능하지만, OHLI에 아예 없는 애니는 검색이 불가능합니다.
 
-    종영 애니 검색은 OHLI만을 검색합니다. OHLI의 검색 API를 이용하기 때문에 fuzzy search 를
-    지원하지 않습니다.
+    종영 애니 포함 검색은 OHLI만을 검색합니다. OHLI의 검색 API를 이용하기 때문에
+    fuzzy search를 지원하지 않습니다.
 
     `{PREFIX}sub 이나즈마 일레븐` (제목이 `'이나즈마 일레븐'` 에 근접하는 것을 검색)
     `{PREFIX}sub 나 히 아` (제목이 `'나 히 아'` 에 근접하는 것을 검색)
-    `{PREFIX}sub --finished aldnoah` (제목에 `'aldnoah'`가 들어가는 완결 애니를 검색)
+    `{PREFIX}sub 히로아카` (OHLI의 애니 별칭 목록에 있는것은 별칭으로도 검색 가능)
+    `{PREFIX}sub --완결 aldnoah` (제목에 `'aldnoah'`가 들어가는 애니 + 완결애니를 검색)
 
     """
 
@@ -155,14 +173,14 @@ async def search_on_air(bot, event: Message, title: str):
             get_weekly_list('http://www.anissia.net/anitime/list', w)
         )
 
-    ohli_results, _ = await asyncio.wait(ohli)
-    anissia_results, _ = await asyncio.wait(anissia)
-    ohli_data: List[Dict[str, Any]] = []
-    anissia_data: List[Dict[str, Any]] = []
+    o_responses, _ = await asyncio.wait(ohli)
+    a_responses, _ = await asyncio.wait(anissia)
+    o_data: List[Dict[str, Any]] = []
+    a_data: List[Dict[str, Any]] = []
 
-    for r in ohli_results:
+    for response in o_responses:
         try:
-            res = r.result()
+            res = response.result()
         except Exception as e:
             await bot.say(
                 event.channel,
@@ -170,20 +188,17 @@ async def search_on_air(bot, event: Message, title: str):
             )
             return
         else:
-            ohli_data.extend(res)
-    for r in anissia_results:
+            o_data.extend(res)
+    for r in a_responses:
         try:
             res = r.result()
-        except Exception as e:
-            await bot.say(
-                event.channel,
-                'Error: {}: {}'.format(e.__class__.__name__, e)
-            )
-            return
+        except Exception:
+            a_data.clear()
+            break
         else:
-            anissia_data.extend(res)
+            a_data.extend(res)
 
-    for ani in ohli_data:
+    for ani in o_data:
         ani['ratio'] = max(
             fuzzy_korean_partial_ratio(
                 title.lower(),
@@ -191,118 +206,110 @@ async def search_on_air(bot, event: Message, title: str):
             ) for a in ani['n']
         )
 
-    ohli_data.sort(key=lambda x: x['ratio'], reverse=True)
+    o_ani = max(o_data, key=lambda x: x['ratio'])
 
-    ohli_ani_result = ohli_data[0]
+    if o_ani['ratio'] > 10:
+        result: List[Sub] = []
 
-    if ohli_ani_result['ratio'] > 10:
-        for ani in anissia_data:
-            ani['ratio'] = max(
-                fuzzy_korean_partial_ratio(a['s'].lower(), ani['s'].lower())
-                for a in ohli_ani_result['n']
-            )
+        o_subs = await get_json(
+            'http://ohli.moe/cap/{}'.format(o_ani['i'])
+        )
 
-        anissia_data.sort(key=lambda x: x['ratio'], reverse=True)
-        anissia_ani_result = anissia_data[0]
+        for sub in o_subs:
+            episode_num = sub['s']
+            if int(math.ceil(episode_num)) == int(episode_num):
+                episode_num = int(episode_num)
+            result.append(Sub(
+                maker=sub['n'],
+                episode_num=episode_num,
+                url=sub['a'],
+                released_at=sub['d'],
+            ))
 
-        if anissia_ani_result['ratio'] > 80:
-            ohli_sub_result = await get_json(
-                'http://ohli.moe/cap/{}'.format(
-                    ohli_ani_result['i']
+        use_anissia = False
+        a_ani = None
+        if a_data:
+            for ani in a_data:
+                ani['ratio'] = max(
+                    fuzzy_korean_partial_ratio(
+                        alias['s'].lower(),
+                        ani['s'].lower()
+                    ) for alias in o_ani['n']
                 )
-            )
-            anissia_sub_result = await get_json(
-                'http://www.anissia.net/anitime/cap?i={}'.format(
-                    anissia_ani_result['i']
+
+                if o_ani['t'] == ani['t']:
+                    ani['ratio'] += 5
+                if o_ani['week'] == ani['week']:
+                    ani['ratio'] += 5
+                if fuzz.ratio(fix_url(ani['l']), o_ani['l']) > 94:
+                    ani['ratio'] += 10
+
+            a_ani = max(a_data, key=lambda x: x['ratio'])
+
+            if a_ani['ratio'] > 80:
+                use_anissia = True
+
+                a_subs = await get_json(
+                    'http://www.anissia.net/anitime/cap?i={}'.format(
+                        a_ani['i']
+                    )
                 )
+
+                for sub in a_subs:
+                    url = fix_url(encode_url(sub['a']))
+                    if o_subs and max(
+                        fuzz.ratio(url.lower(), o_sub['a'].lower())
+                        for o_sub in o_subs
+                    ) > 95:
+                        continue
+                    episode_num = int(sub['s'])/10
+                    if int(math.ceil(episode_num)) == int(episode_num):
+                        episode_num = int(episode_num)
+                    result.append(Sub(
+                        maker=sub['n'],
+                        episode_num=episode_num,
+                        url=url,
+                        released_at=sub['d'],
+                    ))
+
+        title = o_ani['s']
+        dow = DOW[o_ani['week']]
+        time = print_time(o_ani['t'])
+        url = fix_url(o_ani['l'])
+        if use_anissia and a_ani:
+            pretext = (
+                '애니시아와 OHLI의 자막 DB에서 요청하신것과 가장 비슷한 제목의 애니메이션을 '
+                '찾았어요! 양측의 DB의 내용을 종합해서 알려드릴게요!'
             )
-
-            for sub in anissia_sub_result:
-                if ohli_sub_result:
-                    sub['duplicated'] = max(
-                        fuzz.ratio(fix_url(sub['a']).lower(), s['a'].lower())
-                        for s in ohli_sub_result
-                    ) > 95
-                else:
-                    sub['duplicated'] = False
-
-            result: List[Sub] = []
-
-            for sub in ohli_sub_result:
-                episode_num = sub['s']
-                if sub['d'] == '00000000000000':
-                    released_at = datetime.datetime.min
-                else:
-                    released_at = datetime.datetime.strptime(
-                        sub['d'],
-                        '%Y%m%d%H%M%S'
-                    )
-                if int(math.ceil(episode_num)) == int(episode_num):
-                    episode_num = int(episode_num)
-                result.append(Sub(
-                    maker=sub['n'],
-                    episode_num=episode_num,
-                    url=sub['a'],
-                    released_at=released_at,
-                ))
-
-            for sub in anissia_sub_result:
-                if sub['duplicated']:
-                    continue
-                episode_num = int(sub['s'])/10
-                if sub['d'] == '00000000000000':
-                    released_at = datetime.datetime.min
-                else:
-                    released_at = datetime.datetime.strptime(
-                        sub['d'],
-                        '%Y%m%d%H%M%S'
-                    )
-                if int(math.ceil(episode_num)) == int(episode_num):
-                    episode_num = int(episode_num)
-                result.append(Sub(
-                    maker=sub['n'],
-                    episode_num=episode_num,
-                    url=sub['a'],
-                    released_at=released_at,
-                ))
-
-            attachments: List[Attachment] = [
-                Attachment(
-                    fallback=('*{title}* ({dow} {time} '
-                              '/ {genre} / {url})').format(
-                        title=ohli_ani_result['s'],
-                        dow=DOW[ohli_ani_result['week']],
-                        time=print_time(ohli_ani_result['t']),
-                        genre=anissia_ani_result['g'].replace(' ', ''),
-                        url=fix_url(ohli_ani_result['l']),
-                    ),
-                    title=ohli_ani_result['s'],
-                    title_link=fix_url(ohli_ani_result['l'])
-                    if ohli_ani_result['l'] else None,
-                    text='{dow} {time} / {genre}'.format(
-                        dow=DOW[ohli_ani_result['week']],
-                        time=print_time(ohli_ani_result['t']),
-                        genre=anissia_ani_result['g'].replace(' ', ''),
-                    ),
-                    thumb_url=ohli_ani_result['img'] or None,
-                ),
-            ]
-            attachments.extend(make_sub_list(result))
-
-            await bot.api.chat.postMessage(
-                channel=event.channel,
-                attachments=attachments,
-                as_user=True,
-            )
+            genre = a_ani['g'].replace(' ', '')
+            fallback = f'*{title}* ({dow} {time} / {genre} / {url})'
+            text = f'{dow} {time} / {genre}'
         else:
-            await bot.say(
-                event.channel,
-                '입력해주신 키워드로 애니메이션 제목을 특정하지 못하겠어요! '
-                'OHLI에선 {}, Anissia에선 {}이(가) 가장 비슷한 것으로 보여요.'.format(
-                    ohli_ani_result['s'],
-                    anissia_ani_result['s']
-                )
+            pretext = (
+                'OHLI의 자막 DB에서 요청하신것과 가장 비슷한 제목의 애니메이션을 '
+                '찾았어요! 아쉽지만 애니시아에선 찾지 못했어요. OHLI DB의 내용을 알려드릴게요!'
             )
+            fallback = f'*{title}* ({dow} {time} / {url})'
+            text = f'{dow} {time}'
+
+        attachments: List[Attachment] = [
+            Attachment(
+                fallback=fallback,
+                title=title,
+                title_link=url,
+                text=text,
+                thumb_url=o_ani['img'] or None,
+                pretext=pretext,
+            ),
+        ]
+        attachments.extend(make_sub_list(result))
+
+        await bot.api.chat.postMessage(
+            channel=event.channel,
+            attachments=attachments,
+            as_user=True,
+        )
+
     else:
         await bot.say(
             event.channel,
@@ -318,50 +325,50 @@ async def search_finished(bot, event: Message, title: str):
         )
     )
 
-    filtered = list(filter(lambda x: x['status'] == '완', data))
-
-    if filtered:
-        ani = filtered[0]
-        subs = await get_json(
-            'http://ohli.moe/cap/{}'.format(ani['i']))
-        result: List[Sub] = []
-
-        for sub in subs:
-            episode_num = sub['s']
-            if sub['d'] == '00000000000000':
-                released_at = datetime.datetime.min
-            else:
-                released_at = datetime.datetime.strptime(
-                    sub['d'],
-                    '%Y%m%d%H%M%S'
-                )
-            if int(math.ceil(episode_num)) == int(episode_num):
-                episode_num = int(episode_num)
-            result.append(Sub(
-                maker=sub['n'],
-                episode_num=episode_num,
-                url=sub['a'],
-                released_at=released_at,
-            ))
-
-        attachments: List[Attachment] = [
-            Attachment(
-                fallback='*{title}* ({url})'.format(
-                    title=ani['s'],
-                    url=fix_url(ani['l']),
-                ),
-                title=ani['s'],
-                title_link=fix_url(ani['l']) if ani['l'] else None,
-                thumb_url=ani['img'] or None,
-            ),
-        ]
-        attachments.extend(make_sub_list(result))
-
-        await bot.api.chat.postMessage(
-            channel=event.channel,
-            attachments=attachments,
-            as_user=True,
+    if data:
+        chat = await bot.say(
+            event.channel,
+            (
+                f'완결애니를 포함하여 OHLI DB에서 검색한 결과 총 {len(data):,}개의'
+                ' 애니가 검색되었어요!\n검색 결과는 도배의 우려가 있어서 thread로 남길게요!'
+            )
         )
+        ts = chat['ts'] if chat['ok'] else event.event_ts
+        for ani in data:
+            subs = await get_json(
+                'http://ohli.moe/cap/{}'.format(ani['i']))
+            result: List[Sub] = []
+
+            for sub in subs:
+                episode_num = sub['s']
+                if int(math.ceil(episode_num)) == int(episode_num):
+                    episode_num = int(episode_num)
+                result.append(Sub(
+                    maker=sub['n'],
+                    episode_num=episode_num,
+                    url=sub['a'],
+                    released_at=sub['d'],
+                ))
+
+            attachments: List[Attachment] = [
+                Attachment(
+                    fallback='*{title}* ({url})'.format(
+                        title=ani['s'],
+                        url=fix_url(ani['l']),
+                    ),
+                    title=ani['s'],
+                    title_link=fix_url(ani['l']) if ani['l'] else None,
+                    thumb_url=ani['img'] or None,
+                ),
+            ]
+            attachments.extend(make_sub_list(result))
+
+            await bot.api.chat.postMessage(
+                channel=event.channel,
+                attachments=attachments,
+                as_user=True,
+                thread_ts=ts,
+            )
     else:
         await bot.say(
             event.channel,
