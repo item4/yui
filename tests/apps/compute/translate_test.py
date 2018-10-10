3a83dfaf3e704193d2e2e6b5f05eec5038fd24a3
Kim Jin Su <item4_hun@hotmail.com>
2018-10-10 23:00:10 +0900

    Make new command: translate

---
+++ tests/apps/compute/translate_test.py
@@ -0,0 +1,108 @@
+import os
+
+import pytest
+
+from yui.apps.compute.translate import _translate, detect_language, translate
+from yui.event import create_event
+
+from ...util import FakeBot
+
+
+@pytest.fixture()
+def fx_naver_client_id():
+    token = os.getenv('NAVER_CLIENT_ID')
+    if not token:
+        pytest.skip('Can not test this without NAVER_CLIENT_ID envvar')
+    return token
+
+
+@pytest.fixture()
+def fx_naver_client_secret():
+    key = os.getenv('NAVER_CLIENT_SECRET')
+    if not key:
+        pytest.skip('Can not test this without NAVER_CLIENT_SECRET envvar')
+    return key
+
+
+@pytest.fixture()
+def bot(fx_config, fx_naver_client_id, fx_naver_client_secret):
+    fx_config.NAVER_CLIENT_ID = fx_naver_client_id
+    fx_config.NAVER_CLIENT_SECRET = fx_naver_client_secret
+    return FakeBot(fx_config)
+
+
+@pytest.fixture()
+def header(fx_naver_client_id, fx_naver_client_secret):
+    return {
+        'X-Naver-Client-Id': fx_naver_client_id,
+        'X-Naver-Client-Secret': fx_naver_client_secret,
+    }
+
+
+@pytest.mark.asyncio
+async def test_detect_language(header):
+    assert await detect_language(header, '안녕하세요. 제 이름은 유이에요.') == 'ko'
+    assert await detect_language(header, 'こんにちは。私の名前はユです。') == 'ja'
+    assert await detect_language(header, 'Hi. My name is Yui.') == 'en'
+
+
+@pytest.mark.asyncio
+async def test_private_translate_function(header):
+    text = '안녕하세요. 제 이름은 유이에요.'
+    result = 'こんにちは。私の名前はユです。'
+    assert await _translate(header, 'ko', 'ja', text) == result
+
+
+@pytest.mark.asyncio
+async def test_translate_command(bot):
+    bot.add_channel('C1', 'general')
+
+    event = create_event({
+        'type': 'message',
+        'channel': 'C1',
+    })
+
+    await translate(bot, event, None, 'ja', '안녕하세요. 제 이름은 유이에요.'*100)
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '500자 이상의 긴 문장의 번역은 다른 번역기를 사용해주세요!'
+
+    await translate(bot, event, 'php', 'ja', '안녕하세요. 제 이름은 유이에요.')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '원문 언어가 올바르지 않아요!'
+
+    await translate(bot, event, None, 'ja', '?'*100)
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '원문 언어를 추론하는데에 실패했어요!'
+
+    await translate(bot, event, 'ko', 'php', '안녕하세요. 제 이름은 유이에요.')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '결과값 언어가 올바르지 않아요!'
+
+    await translate(bot, event, 'ko', 'ko', '안녕하세요. 제 이름은 유이에요.')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '원문 언어와 결과값 언어가 같아요!'
+
+    await translate(bot, event, 'id', 'th', 'orange')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '인도네시아어에서 태국어로의 번역은 현재 지원되지 않아요!'
+
+    await translate(bot, event, None, 'ja', '안녕하세요. 제 이름은 유이에요.')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '한국어 원문: 안녕하세요. 제 이름은 유이에요.\n'
+        '일본어 번역: こんにちは。私の名前はユです。'
+    )
---
+++ yui/apps/compute/translate.py
@@ -0,0 +1,189 @@
+from typing import Any, Dict, Optional, Set, Tuple
+
+import ujson
+
+from ...box import box
+from ...command import argument, option
+from ...event import Message
+from ...session import client_session
+
+box.assert_config_required('NAVER_CLIENT_ID', str)
+box.assert_config_required('NAVER_CLIENT_SECRET', str)
+
+LANGUAGE_MAP: Dict[Optional[str], Optional[str]] = {
+    None: None,
+    'auto': None,
+    '자동': None,
+    '자동감지': None,
+    'ko': 'ko',
+    'korea': 'ko',
+    'korean': 'ko',
+    '한': 'ko',
+    '한글': 'ko',
+    '한국': 'ko',
+    '한국어': 'ko',
+    'en': 'en',
+    'eng': 'en',
+    'english': 'en',
+    '영': 'en',
+    '영어': 'en',
+    'ja': 'ja',
+    'japan': 'ja',
+    'japanese': 'ja',
+    '일': 'ja',
+    '일어': 'ja',
+    '일본': 'ja',
+    '일본어': 'ja',
+    'zh': 'zh-CN',
+    'zh-cn': 'zh-CN',
+    '중': 'zh-CN',
+    '중국': 'zh-CN',
+    '중국어': 'zh-CN',
+    '중국어간체': 'zh-CN',
+    '중국어 간체': 'zh-CN',
+    '간체': 'zh-CN',
+    '중국어번체': 'zh-TW',
+    '중국어 번체': 'zh-TW',
+    '번체': 'zh-TW',
+    'es': 'es',
+    '스페인': 'es',
+    '스페인어': 'es',
+    'fr': 'fr',
+    '프랑스': 'fr',
+    '프랑스어': 'fr',
+    'vi': 'vi',
+    '베트남': 'vi',
+    '베트남어': 'vi',
+    'th': 'th',
+    '태국': 'th',
+    '태국어': 'th',
+    'id': 'id',
+    '인도네시아': 'id',
+    '인도네시아어': 'id',
+}
+
+LANGUAGE_NAME: Dict[str, str] = {
+    'ko': '한국어',
+    'en': '영어',
+    'ja': '일본어',
+    'zh-CN': '중국어 간체',
+    'zh-TW': '중국어 번체',
+    'es': '스페인어',
+    'fr': '프랑스어',
+    'vi': '베트남어',
+    'th': '태국어',
+    'id': '인도네시아어',
+}
+
+AVAILABLE_COMBINATIONS: Set[Tuple[str, str]] = {
+    ('ko', 'en'),
+    ('ko', 'ja'),
+    ('ko', 'zh-CN'),
+    ('ko', 'zh-TW'),
+    ('ko', 'es'),
+    ('ko', 'fr'),
+    ('ko', 'vi'),
+    ('ko', 'th'),
+    ('ko', 'id'),
+    ('en', 'ja'),
+    ('en', 'fr'),
+}
+AVAILABLE_COMBINATIONS |= {(t, s) for s, t in AVAILABLE_COMBINATIONS}
+
+
+async def detect_language(headers: Dict[str, str], text: str) -> str:
+    url = 'https://openapi.naver.com/v1/papago/detectLangs'
+    async with client_session(headers=headers) as session:
+        async with session.post(url, data={'query': text}) as resp:
+            result: Dict[str, Any] = await resp.json(loads=ujson.loads)
+            return result['langCode']
+
+
+async def _translate(
+    headers: Dict[str, str],
+    source: str,
+    target: str,
+    text: str,
+) -> str:
+    url = 'https://openapi.naver.com/v1/papago/n2mt'
+    data = {
+        'source': source,
+        'target': target,
+        'text': text,
+    }
+    async with client_session(headers=headers) as session:
+        async with session.post(url, data=data) as resp:
+            result: Dict[str, Any] = await resp.json(loads=ujson.loads)
+            return result['message']['result']['translatedText']
+
+
+@box.command('번역', aliases=['translate'])
+@option('--source', '-s')
+@option('--target', '-t', default='ko')
+@argument('text', nargs=-1, concat=True)
+async def translate(bot, event: Message, source, target, text: str):
+    """
+    번역
+
+    파파고 NMT 번역을 활용하여 주어진 문장을 다른 언어로 번역합니다.
+
+    `{PREFIX}번역 ソードアート・オンライン` (주어진 문장의 언어를 자동으로 추론해서 한국어로 번역)
+    `{PREFIX}번역 --source=ja ソードアート・オンライン` (`--source` 옵션으로 원문 언어 지정)
+    `{PREFIX}번역 --target=en ソードアート・オンライン` (`--target` 옵션으로 결과 언어 지정)
+
+    쾌적한 Slack 환경 유지를 위해 번역할 원문 문장은 최대 500자까지만 지원합니다.
+    `--source`/`-s`와 `--target`/`-t` 옵션은 한국어도 인식합니다. (`--target=일본어`)
+
+    """
+
+    if len(text) > 500:
+        await bot.say(
+            event.channel,
+            '500자 이상의 긴 문장의 번역은 다른 번역기를 사용해주세요!'
+        )
+        return
+
+    headers = {
+        'X-Naver-Client-Id': bot.config.NAVER_CLIENT_ID,
+        'X-Naver-Client-Secret': bot.config.NAVER_CLIENT_SECRET,
+    }
+    source_code = LANGUAGE_MAP.get(source, 'error')
+    target_code = LANGUAGE_MAP.get(target, 'error')
+
+    if source_code is None:
+        source_code = await detect_language(headers, text)
+
+    if source_code == 'error':
+        await bot.say(
+            event.channel,
+            '원문 언어가 올바르지 않아요!'
+        )
+    elif source_code == 'unk':
+        await bot.say(
+            event.channel,
+            '원문 언어를 추론하는데에 실패했어요!'
+        )
+    elif target_code is None or target_code == 'error':
+        await bot.say(
+            event.channel,
+            '결과값 언어가 올바르지 않아요!'
+        )
+    elif source_code == target_code:
+        await bot.say(
+            event.channel,
+            '원문 언어와 결과값 언어가 같아요!'
+        )
+    elif (source_code, target_code) not in AVAILABLE_COMBINATIONS:
+        await bot.say(
+            event.channel,
+            f'{LANGUAGE_NAME[source_code]}에서 {LANGUAGE_NAME[target_code]}로의'
+            f' 번역은 현재 지원되지 않아요!'
+        )
+    else:
+        result = await _translate(headers, source_code, target_code, text)
+
+        await bot.say(
+            event.channel,
+            f'{LANGUAGE_NAME[source_code]} 원문: {text}\n'
+            f'{LANGUAGE_NAME[target_code]} 번역: {result}'
+        )
