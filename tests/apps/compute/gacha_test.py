ae1484dd74f979e8f1575273607e6328746eb56a
Kim Jin Su <item4_hun@hotmail.com>
2018-10-28 22:36:54 +0900

    New command for calculate tries to win gacha

--- tests/apps/compute/gacha_test.py
+++ tests/apps/compute/gacha_test.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
+
 import pytest
 
-from yui.apps.compute.gacha import Gacha
+from yui.apps.compute.gacha import Gacha, to_percent
 from yui.event import Message, create_event
 
 from ...util import FakeBot
@@ -62,3 +64,130 @@ async def test_collect(fx_config):
         '30종류의 특전을 모두 모으려면, 평균적으로 120(`119.85`)개의 상품을 구입해야 '
         '전체 수집에 성공할 수 있어요!'
     )
+
+
+@pytest.mark.asyncio
+async def test_challenge(fx_config):
+    bot = FakeBot(fx_config)
+    bot.add_channel('C1', 'general')
+    g = Gacha()
+
+    event: Message = create_event({
+        'type': 'message',
+        'channel': 'C1',
+    })
+
+    await g.challenge(g, bot, event, -1, '0.05')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '성공횟수는 1회 이상, 10,000회 이하로 입력해주세요!'
+
+    await g.challenge(g, bot, event, 9999999, '0.05')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '성공횟수는 1회 이상, 10,000회 이하로 입력해주세요!'
+
+    await g.challenge(g, bot, event, 1, '아무말 대잔치')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '정상적인 확률을 입력해주세요!'
+
+    await g.challenge(g, bot, event, 1, '0.000000001')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '확률값은 0.001% 이상, 99% 이하로 입력해주세요!'
+
+    await g.challenge(g, bot, event, 1, '999999')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '확률값은 0.001% 이상, 99% 이하로 입력해주세요!'
+
+    await g.challenge(g, bot, event, 1000, '0.00001')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == '입력하신 확률값에 비해 성공 횟수가 너무 많아요!'
+
+    await g.challenge(g, bot, event, 1, '0.05')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '5% 확률의 도전을 1번 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n'
+        '- 1번 시도하시면 5% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 6번 시도하시면 26.49% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 14번 시도하시면 51.23% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 28번 시도하시면 76.21% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 59번 시도하시면 95.15% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 90번 시도하시면 99.01% 확률로 목표 횟수만큼 성공할 수 있어요!'
+    )
+
+    await g.challenge(g, bot, event, 1, '3%')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '3% 확률의 도전을 1번 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n'
+        '- 1번 시도하시면 3% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 10번 시도하시면 26.25% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 23번 시도하시면 50.36% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 46번 시도하시면 75.36% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 99번 시도하시면 95.09% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 152번 시도하시면 99.02% 확률로 목표 횟수만큼 성공할 수 있어요!'
+    )
+
+    await g.challenge(g, bot, event, 1, '95%')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '95% 확률의 도전을 1번 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n'
+        '- 1번 시도하시면 95% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 2번 시도하시면 99.74% 확률로 목표 횟수만큼 성공할 수 있어요!'
+    )
+
+    await g.challenge(g, bot, event, 1, '98.00000000%')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '98% 확률의 도전을 1번 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n'
+        '- 1번 시도하시면 98% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 2번 시도하시면 99.96% 확률로 목표 횟수만큼 성공할 수 있어요!'
+    )
+
+    await g.challenge(g, bot, event, 10, '0.1%')
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '0.1% 확률의 도전을 10번 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n'
+        '- 2,964번 시도하시면 0.1% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 7,727번 시도하시면 25% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 9,669번 시도하시면 50% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 11,913번 시도하시면 75% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 15,702번 시도하시면 95% 확률로 목표 횟수만큼 성공할 수 있어요!\n'
+        '- 18,779번 시도하시면 99% 확률로 목표 횟수만큼 성공할 수 있어요!'
+    )
+
+
+def test_to_percent():
+    assert to_percent(Decimal('12.300040000')) == '1230.004'
+    assert to_percent(Decimal('12.300000000')) == '1230'
+    assert to_percent(Decimal('12'), Decimal('1')) == '1200'
--- yui/apps/compute/gacha.py
+++ yui/apps/compute/gacha.py
@@ -1,13 +1,36 @@
 import inspect
 import math
+from decimal import Decimal, InvalidOperation, ROUND_FLOOR
+
+from scipy.stats import nbinom
 
 from sympy.functions.combinatorial.numbers import harmonic
 
 
 from ...box import box, route
-from ...command import argument
+from ...command import argument, option
 from ...event import Message
 from ...transform import value_range
+
+SUCCESSES_MIN = 1
+SUCCESSES_MAX = 10000
+CHANCE_MIN = Decimal('0.00001')
+CHANCE_MAX = Decimal('0.99')
+CHANCES = [
+    Decimal('0.25'),
+    Decimal('0.5'),
+    Decimal('0.75'),
+    Decimal('0.95'),
+    Decimal('0.99'),
+]
+D001 = Decimal('0.01')
+
+
+def to_percent(v: Decimal, q=CHANCE_MIN) -> str:
+    s = str((v * 100).quantize(q, rounding=ROUND_FLOOR))
+    if '.' in s:
+        return s.rstrip('0').rstrip('.')
+    return s
 
 
 class Gacha(route.RouteApp):
@@ -17,6 +40,8 @@ class Gacha(route.RouteApp):
         self.route_list = [
             route.Route(name='수집', callback=self.collect),
             route.Route(name='collect', callback=self.collect),
+            route.Route(name='도전', callback=self.challenge),
+            route.Route(name='challenge', callback=self.challenge),
         ]
 
     def get_short_help(self, prefix: str):
@@ -29,13 +54,83 @@ class Gacha(route.RouteApp):
         해로운 문명, 가챠에 관련된 계산을 도와줍니다.
 
         `{prefix}가챠 수집 10` (총 10종류가 있는 요소를 구입하려면 몇 번 구입해야하는지 계산)
+        `{prefix}가챠 도전 5%` (5% 확률요소의 성공을 위해 필요한 도전 횟수를 계산)
+        `{prefix}가챠 도전 0.1` (10%(`0.1`) 확률요소의 성공을 위해 필요한 도전 횟수를 계산)
+        `{prefix}가챠 도전 --성공 10 3%`""" + """\
+        (3% 확률요소의 10회 성공을 위해 필요한 도전 횟수를 계산)
 
-        `수집` 대신 `collect` 를 사용할 수 있습니다.""")
+        Aliases
+
+        - `수집`대신 `collect`를 사용할 수 있습니다.
+        - `도전`대신 `challenge`를 사용할 수 있습니다.
+        - `도전`에서 `--성공`대신 `--성공횟수`/`--successes`/`-s`를 사용할 수 있습니다.
+        """)
 
     async def fallback(self, bot, event: Message):
         await bot.say(
             event.channel,
             f'Usage: `{bot.config.PREFIX}help 가챠`'
+        )
+
+    @option('--successes', '-s', '--성공횟수', '--성공', default=1)
+    @argument('chance')
+    async def challenge(
+        self,
+        bot,
+        event: Message,
+        successes: int,
+        chance: str,
+    ):
+        if successes < SUCCESSES_MIN or successes > SUCCESSES_MAX:
+            await bot.say(
+                event.channel,
+                f'성공횟수는 {SUCCESSES_MIN}회 이상,'
+                f' {SUCCESSES_MAX:,}회 이하로 입력해주세요!'
+            )
+            return
+        try:
+            if chance.endswith('%'):
+                p = Decimal(chance[:-1]) / 100
+            else:
+                p = Decimal(chance)
+        except InvalidOperation:
+            await bot.say(
+                event.channel,
+                '정상적인 확률을 입력해주세요!'
+            )
+            return
+        if p < CHANCE_MIN or p > CHANCE_MAX:
+            await bot.say(
+                event.channel,
+                f'확률값은 {to_percent(CHANCE_MIN)}% 이상,'
+                f' {to_percent(CHANCE_MAX)}% 이하로 입력해주세요!'
+            )
+            return
+        if p / successes < CHANCE_MIN:
+            await bot.say(
+                event.channel,
+                '입력하신 확률값에 비해 성공 횟수가 너무 많아요!'
+            )
+            return
+        counts = {
+            int(math.ceil(
+                nbinom.ppf(float(q), successes, float(p))
+            ))
+            for q in filter(lambda x: x >= p, CHANCES + [p])
+        }
+        results = [
+            (x, Decimal(str(nbinom.cdf(x, successes, float(p)))))
+            for x in sorted(counts)
+        ]
+        text = '\n'.join(
+            f'- {tries+successes:,}번 시도하시면 {to_percent(ch, D001)}% 확률로'
+            f' 목표 횟수만큼 성공할 수 있어요!'
+            for tries, ch in results
+        )
+        await bot.say(
+            event.channel,
+            f'{to_percent(p)}% 확률의 도전을 {successes:,}번'
+            f' 성공시키려면 몇 회의 도전이 필요한지 알려드릴게요!\n{text}'
         )
 
     @argument('n', type_=int, transform_func=value_range(1, 128),
