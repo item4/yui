24de7f971aadd365a76a2015f2459f556b2bc6a4
Kim Jin Su <item4_hun@hotmail.com>
2018-12-30 18:02:46 +0900

    New fun handler: code review

---
+++ tests/apps/fun/code_test.py
@@ -0,0 +1,56 @@
+import pytest
+
+import ujson
+
+from yui.apps.fun.code import code_review, write_code_review
+
+from ...util import FakeBot
+
+
+@pytest.mark.asyncio
+async def test_write_code_review():
+    bot = FakeBot()
+    bot.add_channel('C1', 'general')
+    bot.add_user('U1', 'item4')
+
+    event = bot.create_message('C1', 'U1', text='램지 코드 리뷰')
+    await write_code_review(bot, event, seed=100)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+
+    assert said.data['as_user'] == '0'
+
+    attachments = ujson.loads(said.data['attachments'])
+
+    assert len(attachments) == 1
+    assert attachments[0]['fallback'] == attachments[0]['image_url'] == \
+        'https://i.imgur.com/btkBRvc.png'
+
+
+@pytest.mark.asyncio
+async def test_code_review():
+    bot = FakeBot()
+    bot.add_channel('C1', 'general')
+    bot.add_user('U1', 'item4')
+
+    event = bot.create_message('C1', 'U1', text='코드 리뷰')
+
+    assert await code_review(bot, event)
+
+    assert not bot.call_queue
+
+    event = bot.create_message('C1', 'U1', text='램지 코드 리뷰')
+
+    assert not await code_review(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert len(ujson.loads(said.data['attachments'])) == 1
+    assert said.data['as_user'] == '0'
+
+    event = bot.create_message('C1', 'U1', text='램지 코드 리뷰')
+
+    assert await code_review(bot, event)
---
+++ yui/apps/fun/code.py
@@ -0,0 +1,54 @@
+import random
+import re
+from datetime import timedelta
+
+from ...api import Attachment
+from ...box import box
+from ...event import Message
+from ...utils.datetime import now
+
+COOLTIME = timedelta(minutes=15)
+PATTERN = re.compile(
+    r'(?:고든|코드)?\s*램지님?\s*[제내저이]?\s*(?:코드|[PM]R|풀리퀘)좀?\s*리뷰',
+)
+
+ICON_URL = 'https://i.imgur.com/bGVUlSp.jpg'
+IMAGES = {
+    'https://i.imgur.com/btkBRvc.png': 2,  # 루왁커피
+    'https://i.imgur.com/v3bu01T.png': 2,  # 스파게티
+    'https://i.imgur.com/ODOVLQA.png': 1,  # 지사제
+}
+
+
+async def write_code_review(bot, event: Message, *, seed=None):
+    random.seed(seed)
+    image_url = random.choices(*zip(*IMAGES.items()))[0]
+    random.seed(None)
+    await bot.api.chat.postMessage(
+        channel=event.channel,
+        attachments=[
+            Attachment(
+                fallback=image_url,
+                image_url=image_url,
+            ),
+        ],
+        as_user=False,
+        icon_url=ICON_URL,
+        username='코드램지',
+    )
+
+
+@box.on(Message)
+async def code_review(bot, event: Message):
+    if PATTERN.search(event.text.upper()):
+        now_dt = now()
+        last_call = code_review.last_call.get(event.channel.id)
+        cond = last_call is None
+        if not cond:
+            cond = last_call + COOLTIME <= now_dt
+
+        if cond:
+            await write_code_review(bot, event)
+            code_review.last_call[event.channel.id] = now_dt
+            return False
+    return True
