89c0300090b18b28df0e6d678fac3fe1a86a38dc
Kim Jin Su <item4_hun@hotmail.com>
2019-03-31 11:00:13 +0900

    Stop using DNS over HTTPS because of API broken of aiodns

--- yui/session.py
+++ yui/session.py
@@ -1,28 +1,3 @@
-import socket
+import aiohttp
 
-from aiohttp.resolver import AsyncResolver
-
-from aiohttp_doh import ClientSession
-
-import ujson
-
-
-class YuiAsyncResolver(AsyncResolver):
-    """DNS resolver with aiodns but forced AF_INET family."""
-
-    def resolve(self, host, port=0, family=socket.AF_INET):
-        return super(
-            YuiAsyncResolver,
-            self,
-        ).resolve(host, port, socket.AF_INET)
-
-
-def client_session(*args, **kwargs) -> ClientSession:
-    """aiohttp.client.ClientSession with DNS over HTTPS"""
-
-    return ClientSession(
-        *args,
-        **kwargs,
-        json_loads=ujson.loads,
-        resolver_class=YuiAsyncResolver,
-    )
+client_session = aiohttp.ClientSession
