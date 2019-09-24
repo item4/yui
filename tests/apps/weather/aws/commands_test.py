3e3357352ac43f39baa8ec0645213ac4560cd092
Kim Jin Su <item4_hun@hotmail.com>
2019-09-24 12:42:54 +0900

    Fix broken weather command tests

--- tests/apps/weather/aws/commands_test.py
+++ tests/apps/weather/aws/commands_test.py
@@ -1,5 +1,3 @@
-import copy
-
 import pytest
 
 from yui.apps.shared.cache import JSONCache
@@ -17,7 +15,7 @@ async def test_aws(fx_sess):
 
     event = bot.create_message('C1', 'U1', ts='1234.56')
 
-    await aws(bot, event, fx_sess, False, False, '서울')
+    await aws(bot, event, fx_sess, '서울')
 
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
@@ -88,7 +86,7 @@ async def test_aws(fx_sess):
             },
             {
                 "id": 2,
-                "name": "가나",
+                "name": "강남2",
                 "height": 1234,
                 "rain": {
                     "is_raining": "Clear",
@@ -112,7 +110,7 @@ async def test_aws(fx_sess):
                 },
                 "humidity": 55,
                 "atmospheric": 2345.67,
-                "address": "지구 반대편 가나",
+                "address": "강남2",
             },
         ]
     }
@@ -122,7 +120,7 @@ async def test_aws(fx_sess):
         fx_sess.add(cache)
 
     # not found
-    await aws(bot, event, fx_sess, False, False, '부천')
+    await aws(bot, event, fx_sess, '부천')
 
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
@@ -130,8 +128,10 @@ async def test_aws(fx_sess):
     assert said.data['text'] == '검색결과가 없어요!'
     assert 'thread_ts' not in said.data
 
-    # rain
-    await aws(bot, event, fx_sess, False, False, '서울')
+    # found 1
+    await aws(bot, event, fx_sess, '서울')
+
+    assert len(bot.call_queue) == 1
 
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
@@ -145,8 +145,10 @@ async def test_aws(fx_sess):
     assert said.data['icon_emoji'] == ':umbrella_with_rain_drops:'
     assert 'thread_ts' not in said.data
 
-    # snow
-    await aws(bot, event, fx_sess, False, False, '강남')
+    # found 2+
+    await aws(bot, event, fx_sess, '강남')
+
+    assert len(bot.call_queue) == 2
 
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
@@ -158,127 +160,16 @@ async def test_aws(fx_sess):
     )
     assert said.data['username'] == '강남 날씨'
     assert said.data['icon_emoji'] == ':snowflake:'
-    assert 'thread_ts' not in said.data
-
-    # sun
-    await aws(bot, event, fx_sess, False, False, '가나')
+    assert said.data['thread_ts'] == '1234.56'
 
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
     assert said.data['text'] == (
-        '[2018년 12월 30일 13시 16분@가나/지구 반대편 가나]'
+        '[2018년 12월 30일 13시 16분@강남2/강남2]'
         ' 강수: 아니오'
         ' / 12.34℃ / 바람: 남남서 11.11㎧ / 습도: 55% / 해면기압: 2345.67hPa'
     )
-    assert said.data['username'] == '가나 날씨'
+    assert said.data['username'] == '강남2 날씨'
     assert said.data['icon_emoji'] == ':sunny:'
-    assert 'thread_ts' not in said.data
-
-    # moon
-    new_body = copy.deepcopy(cache.body)
-    new_body['observed_at'] = '2018-12-30T22:16:00+0900'
-    cache.body = new_body
-    with fx_sess.begin():
-        fx_sess.add(cache)
-
-    await aws(bot, event, fx_sess, False, False, '가나')
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['text'] == (
-        '[2018년 12월 30일 22시 16분@가나/지구 반대편 가나]'
-        ' 강수: 아니오'
-        ' / 12.34℃ / 바람: 남남서 11.11㎧ / 습도: 55% / 해면기압: 2345.67hPa'
-    )
-    assert said.data['username'] == '가나 날씨'
-    assert said.data['icon_emoji'] == ':crescent_moon:'
-    assert 'thread_ts' not in said.data
-
-    # include address
-    await aws(bot, event, fx_sess, False, True, '서울')
-
-    assert len(bot.call_queue) == 2
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '서울 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '강남 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    await aws(bot, event, fx_sess, False, True, '가나')
-
-    assert len(bot.call_queue) == 1
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '가나 날씨'
-    assert 'thread_ts' not in said.data
-
-    # fuzzy
-    await aws(bot, event, fx_sess, True, False, '강남')
-
-    assert len(bot.call_queue) == 2
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '강남 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '가나 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    await aws(bot, event, fx_sess, True, False, '서울')
-
-    assert len(bot.call_queue) == 1
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '서울 날씨'
-    assert 'thread_ts' not in said.data
-
-    # fuzzy + include address
-    await aws(bot, event, fx_sess, True, True, '서울')
-
-    assert len(bot.call_queue) == 2
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '서울 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '강남 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    await aws(bot, event, fx_sess, True, True, '가나')
-
-    assert len(bot.call_queue) == 2
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '가나 날씨'
-    assert said.data['thread_ts'] == event.ts
-
-    said = bot.call_queue.pop(0)
-    assert said.method == 'chat.postMessage'
-    assert said.data['channel'] == 'C1'
-    assert said.data['username'] == '강남 날씨'
-    assert said.data['thread_ts'] == event.ts
+    assert said.data['thread_ts'] == '1234.56'
