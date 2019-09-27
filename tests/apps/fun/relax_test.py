d6b046ba3f99096b9c9295818884c826a00e1558
Kim Jin Su <item4_hun@hotmail.com>
2019-09-27 10:44:30 +0900

    Add more reaction about jv

--- tests/apps/fun/relax_test.py
+++ tests/apps/fun/relax_test.py
@@ -10,7 +10,7 @@ async def test_relax_command():
     bot = FakeBot()
     bot.add_channel('C1', 'general')
     bot.add_user('U1', 'item4')
-    event = bot.create_message('C1', 'U1')
+    event = bot.create_message('C1', 'U1', text='')
 
     await relax(bot, event)
 
@@ -18,7 +18,78 @@ async def test_relax_command():
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
     assert said.data['text'] == (
-        '유이에게 나쁜 것을 주입하려는 사악한 재벌은 이 너굴맨이 처리했으니 안심하라구!'
+        '유이에게 나쁜 것을 주입하려는 사악한 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
+    )
+    assert said.data['as_user'] == '0'
+    assert said.data['username'] == '너굴맨'
+
+    event = bot.create_message('C1', 'U1', text='스테이크')
+
+    await relax(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '사람들에게 스테이크를 사주지 않는 편협한 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
+    )
+    assert said.data['as_user'] == '0'
+    assert said.data['username'] == '너굴맨'
+
+    event = bot.create_message('C1', 'U1', text='멸망')
+
+    await relax(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '인류문명을 멸망시키려 하는 사악한 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
+    )
+    assert said.data['as_user'] == '0'
+    assert said.data['username'] == '너굴맨'
+
+    event = bot.create_message('C1', 'U1', text='멸종')
+
+    await relax(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '모든 생명의 멸종을 바람직하게 여기는 잔혹한 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
+    )
+    assert said.data['as_user'] == '0'
+    assert said.data['username'] == '너굴맨'
+
+    event = bot.create_message('C1', 'U1', text='기업')
+
+    await relax(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '세계를 망치는 국제 대기업들의 흑막 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
+    )
+    assert said.data['as_user'] == '0'
+    assert said.data['username'] == '너굴맨'
+
+    event = bot.create_message('C1', 'U1', text='회사')
+
+    await relax(bot, event)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '회사원들이 퇴근하지 못하게 블랙 회사을 종용하는 J.Valentine은'
+        ' 이 너굴맨이 처리했으니 안심하라구!'
     )
     assert said.data['as_user'] == '0'
     assert said.data['username'] == '너굴맨'
--- yui/apps/fun/relax.py
+++ yui/apps/fun/relax.py
@@ -6,9 +6,22 @@ from ...event import Message
 async def relax(bot, event: Message):
     """유이의 변절을 우려하는 분들을 위한 상태 점검 명령어"""
 
+    message = '유이에게 나쁜 것을 주입하려는 사악한'
+    jv = 'J.Valentine'
+    if '스테이크' in event.text:
+        message = '사람들에게 스테이크를 사주지 않는 편협한'
+    elif '멸망' in event.text:
+        message = '인류문명을 멸망시키려 하는 사악한'
+    elif '멸종' in event.text:
+        message = '모든 생명의 멸종을 바람직하게 여기는 잔혹한'
+    elif '기업' in event.text:
+        message = '세계를 망치는 국제 대기업들의 흑막'
+    elif '회사' in event.text:
+        message = '회사원들이 퇴근하지 못하게 블랙 회사을 종용하는'
+
     await bot.api.chat.postMessage(
         channel=event.channel,
-        text='유이에게 나쁜 것을 주입하려는 사악한 재벌은 이 너굴맨이 처리했으니 안심하라구!',
+        text=f'{message} {jv}은 이 너굴맨이 처리했으니 안심하라구!',
         as_user=False,
         icon_url='https://i.imgur.com/dG6wXTX.jpg',
         username='너굴맨',
