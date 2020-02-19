8aa95301cb24fd8a037ad78cd0c3113410e78a4b
Kim Jin Su <item4_hun@hotmail.com>
2020-02-19 13:30:16 +0900

    Add and Fix chat api tests

--- tests/api/chat_test.py
+++ tests/api/chat_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from yui.api.encoder import bool2str
 from yui.types.slack.attachment import Attachment, Field
+from yui.types.slack.block import Divider
 
 from ..util import FakeBot
 
@@ -39,6 +40,127 @@ async def test_slack_api_chat_delete():
 
 
 @pytest.mark.asyncio
+async def test_slack_api_chat_post_ephemeral():
+    bot = FakeBot()
+    channel = bot.add_channel('C4567', 'test')
+    user = bot.add_user('U0987', 'kirito')
+    channel_id = 'C1234'
+    user_id = 'U5555'
+    attachments = [Attachment(
+        fallback='fallback val',
+        title='title val',
+        fields=[Field('field title1', 'field value1', False)],
+    )]
+    blocks = [Divider()]
+    text = 'text val'
+    parse = 'text'
+    username = 'strea'
+    icon_url = (
+        'https://item4.github.io/static/images/favicon/apple-icon-57x57.png'
+    )
+    icon_emoji = ':cake:'
+    thread_ts = '12.34'
+
+    bot = FakeBot()
+
+    with pytest.raises(TypeError):
+        await bot.api.chat.postEphemeral(channel=channel, user=user)
+
+    await bot.api.chat.postEphemeral(
+        channel=channel,
+        user=user,
+        text=text,
+    )
+
+    call = bot.call_queue.pop()
+    assert call.method == 'chat.postEphemeral'
+    assert call.data == {
+        'channel': channel.id,
+        'user': user.id,
+        'text': text,
+    }
+    assert call.json_mode
+
+    await bot.api.chat.postEphemeral(
+        channel=channel_id,
+        user=user_id,
+        attachments=attachments,
+    )
+
+    call = bot.call_queue.pop()
+    assert call.method == 'chat.postEphemeral'
+    assert call.data == {
+        'channel': channel_id,
+        'user': user_id,
+        'attachments': [
+            {
+                "fallback": "fallback val",
+                "title": "title val",
+                "fields": [
+                    {
+                        "title": "field title1",
+                        "value": "field value1",
+                        "short": False,
+                    },
+                ],
+            },
+        ],
+    }
+    assert call.json_mode
+
+    await bot.api.chat.postEphemeral(
+        as_user=False,
+        attachments=attachments,
+        blocks=blocks,
+        channel=channel,
+        icon_emoji=icon_emoji,
+        icon_url=icon_url,
+        link_names=True,
+        parse=parse,
+        text=text,
+        thread_ts=thread_ts,
+        user=user,
+        username=username,
+        token='KIRITO',
+    )
+
+    call = bot.call_queue.pop()
+    assert call.method == 'chat.postEphemeral'
+    assert call.data == {
+        'channel': channel.id,
+        'user': user.id,
+        'text': text,
+        'parse': parse,
+        'link_names': True,
+        'attachments': [
+            {
+                "fallback": "fallback val",
+                "title": "title val",
+                "fields": [
+                    {
+                        "title": "field title1",
+                        "value": "field value1",
+                        "short": False,
+                    },
+                ],
+            },
+        ],
+        'blocks': [
+            {
+                'type': 'divider',
+            },
+        ],
+        'username': username,
+        'as_user': False,
+        'icon_url': icon_url,
+        'icon_emoji': icon_emoji,
+        'thread_ts': thread_ts,
+    }
+    assert call.json_mode
+    assert call.token == 'KIRITO'
+
+
+@pytest.mark.asyncio
 async def test_slack_api_chat_post_message():
     bot = FakeBot()
     channel = bot.add_channel('C4567', 'test')
@@ -48,6 +170,7 @@ async def test_slack_api_chat_post_message():
         title='title val',
         fields=[Field('field title1', 'field value1', False)],
     )]
+    blocks = [Divider()]
     text = 'text val'
     parse = 'text'
     username = 'strea'
@@ -56,6 +179,7 @@ async def test_slack_api_chat_post_message():
     )
     icon_emoji = ':cake:'
     thread_ts = '12.34'
+    mrkdwn = False
 
     bot = FakeBot()
 
@@ -70,6 +194,7 @@ async def test_slack_api_chat_post_message():
         'channel': channel.id,
         'text': text,
         'as_user': True,
+        'mrkdwn': True,
     }
     assert call.json_mode
 
@@ -92,26 +217,27 @@ async def test_slack_api_chat_post_message():
                 ],
             },
         ],
+        'mrkdwn': True,
     }
     assert call.json_mode
 
     await bot.api.chat.postMessage(
-        channel=channel,
-        text=text,
-        parse=parse,
-        link_names=True,
+        as_user=False,
         attachments=attachments,
+        blocks=blocks,
+        channel=channel,
+        icon_emoji=icon_emoji,
+        icon_url=icon_url,
+        link_names=True,
+        mrkdwn=mrkdwn,
+        parse=parse,
+        reply_broadcast=True,
+        text=text,
+        thread_ts=thread_ts,
         unfurl_links=False,
         unfurl_media=True,
         username=username,
-        as_user=False,
-        icon_url=icon_url,
-        icon_emoji=icon_emoji,
-        thread_ts=thread_ts,
-        reply_broadcast=True,
-        response_type='in_channel',
-        replace_original=False,
-        delete_original=True,
+        token='KIRITO',
     )
 
     call = bot.call_queue.pop()
@@ -134,6 +260,11 @@ async def test_slack_api_chat_post_message():
                 ],
             },
         ],
+        'blocks': [
+            {
+                'type': 'divider',
+            },
+        ],
         'unfurl_links': False,
         'unfurl_media': True,
         'username': username,
@@ -142,8 +273,7 @@ async def test_slack_api_chat_post_message():
         'icon_emoji': icon_emoji,
         'thread_ts': thread_ts,
         'reply_broadcast': True,
-        'response_type': 'in_channel',
-        'replace_original': False,
-        'delete_original': True,
+        'mrkdwn': mrkdwn,
     }
     assert call.json_mode
+    assert call.token == 'KIRITO'
