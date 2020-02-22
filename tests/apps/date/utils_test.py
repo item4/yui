ca46838e5b65192b0b3ee96e351f27b34de6849e
Kim Jin Su <item4_hun@hotmail.com>
2020-02-22 22:11:15 +0900

    Add test cases for date app utils

--- tests/apps/date/utils_test.py
+++ tests/apps/date/utils_test.py
@@ -2,7 +2,12 @@ import datetime
 
 import pytest
 
-from yui.apps.date.utils import APIDoesNotSupport, get_holiday_names
+from yui.apps.date.utils import (
+    APIDoesNotSupport,
+    get_holiday_names,
+    weekend_loading_box,
+    weekend_loading_percent,
+)
 
 
 @pytest.mark.asyncio
@@ -23,3 +28,79 @@ async def test_get_holiday_names():
 
     with pytest.raises(APIDoesNotSupport):
         await get_holiday_names(unsupported)
+
+
+def test_weekend_loading_percent():
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 1)) == 0.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 2)) == 20.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 3)) == 40.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 4)) == 60.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 5)) == 80.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 6)) == 100.0
+    assert weekend_loading_percent(datetime.datetime(2020, 6, 7)) == 100.0
+
+
+def test_weekend_loading_box():
+    assert weekend_loading_box(0.0) == (
+        '[□□□□□□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(5.0) == (
+        '[■□□□□□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(10.0) == (
+        '[■■□□□□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(15.0) == (
+        '[■■■□□□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(20.0) == (
+        '[■■■■□□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(25.0) == (
+        '[■■■■■□□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(30.0) == (
+        '[■■■■■■□□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(35.0) == (
+        '[■■■■■■■□□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(40.0) == (
+        '[■■■■■■■■□□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(45.0) == (
+        '[■■■■■■■■■□□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(50.0) == (
+        '[■■■■■■■■■■□□□□□□□□□□]'
+    )
+    assert weekend_loading_box(55.0) == (
+        '[■■■■■■■■■■■□□□□□□□□□]'
+    )
+    assert weekend_loading_box(60.0) == (
+        '[■■■■■■■■■■■■□□□□□□□□]'
+    )
+    assert weekend_loading_box(65.0) == (
+        '[■■■■■■■■■■■■■□□□□□□□]'
+    )
+    assert weekend_loading_box(70.0) == (
+        '[■■■■■■■■■■■■■■□□□□□□]'
+    )
+    assert weekend_loading_box(75.0) == (
+        '[■■■■■■■■■■■■■■■□□□□□]'
+    )
+    assert weekend_loading_box(80.0) == (
+        '[■■■■■■■■■■■■■■■■□□□□]'
+    )
+    assert weekend_loading_box(85.0) == (
+        '[■■■■■■■■■■■■■■■■■□□□]'
+    )
+    assert weekend_loading_box(90.0) == (
+        '[■■■■■■■■■■■■■■■■■■□□]'
+    )
+    assert weekend_loading_box(95.0) == (
+        '[■■■■■■■■■■■■■■■■■■■□]'
+    )
+    assert weekend_loading_box(100.0) == (
+        '[■■■■■■■■■■■■■■■■■■■■]'
+    )
