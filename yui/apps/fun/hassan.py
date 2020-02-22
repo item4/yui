c67b800c53bbb4160beeac333ab7a650d207e400
Kim Jin Su <item4_hun@hotmail.com>
2020-02-22 21:59:14 +0900

    Use link helper instead of direct username

--- yui/apps/fun/hassan.py
+++ yui/apps/fun/hassan.py
@@ -2,6 +2,7 @@ import re
 
 from ...box import box
 from ...event import Message
+from ...utils import format
 
 HASSAN_TRIGGER_PATTERN = re.compile(r'^똑바로\s*서라\s*[,\.!]*\s*유이')
 
@@ -9,12 +10,9 @@ HASSAN_TRIGGER_PATTERN = re.compile(r'^똑바로\s*서라\s*[,\.!]*\s*유이')
 @box.on(Message)
 async def hassan(bot, event: Message):
     if HASSAN_TRIGGER_PATTERN.search(event.text):
-        try:
-            await bot.say(
-                event.channel,
-                '저한테 왜 그러세요 @{}님?'.format(event.user.name)
-            )
-        except AttributeError:
-            pass
+        await bot.say(
+            event.channel,
+            f'저한테 왜 그러세요 {format.link(event.user)}님?',
+        )
         return False
     return True
