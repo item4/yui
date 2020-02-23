5a0e9a027b09f64f3d1df71b8b21c9080cfc878e
Kim Jin Su <item4_hun@hotmail.com>
2020-02-23 12:07:01 +0900

    Test if source == target == korean on translate

--- tests/apps/compute/translate_test.py
+++ tests/apps/compute/translate_test.py
@@ -59,13 +59,13 @@ async def test_translate_command(bot):
 
     event = bot.create_message('C1', 'U1')
 
-    await translate(bot, event, None, 'ja', '안녕하세요. 제 이름은 유이에요.'*100)
+    await translate(bot, event, None, 'ja', '안녕하세요. 제 이름은 YUI에요.'*100)
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
     assert said.data['text'] == '500자 이상의 긴 문장의 번역은 다른 번역기를 사용해주세요!'
 
-    await translate(bot, event, 'php', 'ja', '안녕하세요. 제 이름은 유이에요.')
+    await translate(bot, event, 'php', 'ja', '안녕하세요. 제 이름은 YUI에요.')
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
@@ -77,13 +77,13 @@ async def test_translate_command(bot):
     assert said.data['channel'] == 'C1'
     assert said.data['text'] == '원문 언어를 추론하는데에 실패했어요!'
 
-    await translate(bot, event, 'ko', 'php', '안녕하세요. 제 이름은 유이에요.')
+    await translate(bot, event, 'ko', 'php', '안녕하세요. 제 이름은 YUI에요.')
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
     assert said.data['text'] == '결과값 언어가 올바르지 않아요!'
 
-    await translate(bot, event, 'en', 'en', '안녕하세요. 제 이름은 유이에요.')
+    await translate(bot, event, 'en', 'en', '안녕하세요. 제 이름은 YUI에요.')
     said = bot.call_queue.pop(0)
     assert said.method == 'chat.postMessage'
     assert said.data['channel'] == 'C1'
@@ -102,4 +102,13 @@ async def test_translate_command(bot):
     assert said.data['text'] == (
         '한국어 원문: 안녕하세요. 제 이름은 YUI에요.\n'
         '일본어 번역: こんにちは。私の名前はYUIです。'
+    )
+
+    await translate(bot, event, 'ko', 'ko', '안녕하세요. 제 이름은 YUI에요.')
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == (
+        '한국어 원문: 안녕하세요. 제 이름은 YUI에요.\n'
+        '영어 번역: Hello. My name is YUI.'
     )
