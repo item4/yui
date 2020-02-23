cb373985465edcfc5410c3b1e3f99842cd3504c6
Kim Jin Su <item4_hun@hotmail.com>
2020-02-23 09:10:19 +0900

    Change formatting and make test cases for memo

---
+++ tests/apps/info/memo/commands_test.py
@@ -0,0 +1,118 @@
+import pytest
+
+from yui.apps.info.memo.commands import memo_add, memo_delete, memo_show
+from yui.apps.info.memo.models import Memo
+from yui.orm.utils import get_count
+
+from ....util import FakeBot
+
+
+@pytest.mark.asyncio
+async def test_memo_flow(fx_sess):
+    bot = FakeBot()
+    keyword1 = '키리토'
+    keyword2 = '밥'
+    text1 = '키리가야 카즈토의 게임 아이디'
+    text2 = '귀엽다'
+    text3 = '먹어야한다'
+
+    bot.add_channel('C1', 'test')
+    bot.add_user('U1', 'tester')
+    event = bot.create_message('C1', 'U1')
+
+    await memo_show(bot, event, fx_sess, keyword1)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword1}`란 이름을 가진 기억 레코드가 없어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword1),
+    ) == 0
+
+    await memo_show(bot, event, fx_sess, keyword2)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword2}`이란 이름을 가진 기억 레코드가 없어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword2),
+    ) == 0
+
+    await memo_add(bot, event, fx_sess, keyword1, text1)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword1}`로 기억 레코드를 생성했어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword1),
+    ) == 1
+
+    await memo_add(bot, event, fx_sess, keyword2, text3)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword2}`으로 기억 레코드를 생성했어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword2),
+    ) == 1
+
+    await memo_add(bot, event, fx_sess, keyword1, text2)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword1}`로 기억 레코드를 생성했어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword1),
+    ) == 2
+
+    await memo_show(bot, event, fx_sess, keyword1)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword1}`: {text1} | {text2}'
+
+    await memo_show(bot, event, fx_sess, keyword2)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword2}`: {text3}'
+
+    await memo_delete(bot, event, fx_sess, keyword1)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword1}`에 관한 기억 레코드를 모두 삭제했어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword1),
+    ) == 0
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword2),
+    ) == 1
+
+    await memo_delete(bot, event, fx_sess, keyword2)
+
+    said = bot.call_queue.pop(0)
+    assert said.method == 'chat.postMessage'
+    assert said.data['channel'] == 'C1'
+    assert said.data['text'] == f'`{keyword2}`에 관한 기억 레코드를 모두 삭제했어요!'
+
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword1),
+    ) == 0
+    assert get_count(
+        fx_sess.query(Memo).filter_by(keyword=keyword2),
+    ) == 0
--- yui/apps/info/memo/commands.py
+++ yui/apps/info/memo/commands.py
@@ -4,18 +4,19 @@ from .models import Memo
 from ....box import box
 from ....command import argument
 from ....event import Message
+from ....utils import format
 from ....utils.datetime import now
 
 
 @box.command('기억')
 @argument('keyword')
-@argument('text', nargs=-1, concat=True)
+@argument('text')
 async def memo_add(bot, event: Message, sess, keyword: str, text: str):
     """
     기억 레코드 생성
 
     `{PREFIX}기억 키리토 귀엽다` (`키리토`라는 단어를 `귀엽다`라는 내용으로 저장)
-    `{PREFIX}기억 "키리가야 카즈토" 키리토의 본명` (`키리가야 카즈토`에 대한 정보를 저장)
+    `{PREFIX}기억 "키리가야 카즈토" "키리토의 본명"` (`키리가야 카즈토`에 대한 정보를 저장)
 
     """
 
@@ -30,8 +31,8 @@ async def memo_add(bot, event: Message, sess, keyword: str, text: str):
 
     await bot.say(
         event.channel,
-        '`{}`{} 기억 레코드를 생성했어요!'.format(
-            keyword,
+        '{}{} 기억 레코드를 생성했어요!'.format(
+            format.code(keyword),
             tossi.pick(keyword, '(으)로'),
         )
     )
@@ -48,18 +49,18 @@ async def memo_show(bot, event: Message, sess, keyword: str):
     """
 
     memos = sess.query(Memo).filter_by(keyword=keyword)\
-        .order_by(Memo.created_datetime).all()
+        .order_by(Memo.created_at.asc()).all()
 
     if memos:
         await bot.say(
             event.channel,
-            f'`{keyword}`: ' + ' | '.join(x.text for x in memos)
+            f'{format.code(keyword)}: ' + ' | '.join(x.text for x in memos)
         )
     else:
         await bot.say(
             event.channel,
-            '`{}`{} 이름을 가진 기억 레코드가 없어요!'.format(
-                keyword,
+            '{}{} 이름을 가진 기억 레코드가 없어요!'.format(
+                format.code(keyword),
                 tossi.pick(keyword, '(이)란'),
             )
         )
@@ -79,5 +80,5 @@ async def memo_delete(bot, event: Message, sess, keyword: str):
 
     await bot.say(
         event.channel,
-        f'`{keyword}`에 관한 기억 레코드를 모두 삭제했어요!'
+        f'{format.code(keyword)}에 관한 기억 레코드를 모두 삭제했어요!'
     )
