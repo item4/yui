3ca7537395f825c4985ec8e96d05651e47fafd59
Kim Jin Su <item4_hun@hotmail.com>
2020-10-27 13:04:14 +0900

    Set limit for say text

--- yui/bot.py
+++ yui/bot.py
@@ -301,7 +301,7 @@ class Bot:
         """Shortcut for bot saying."""
 
         coro = self.api.chat.postMessage(
-            channel, text, as_user=True, link_names=True, **kwargs
+            channel, text[:300], as_user=True, link_names=True, **kwargs
         )
         if retry_until_send:
             return await retry(coro)
