dfc156829ba1d7b940b696fc623bada5b99f4635
Kim Jin Su <item4_hun@hotmail.com>
2020-12-10 22:56:08 +0900

    Optimize docker image

--- ci/docker_login.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if [ "${TRAVIS_BRANCH}" = "main" ] && [ "${TRAVIS_PULL_REQUEST}" = false ]; then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-fi
--- ci/docker_push.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if [ "${TRAVIS_BRANCH}" = "main" ] && [ "${TRAVIS_PULL_REQUEST}" = false ]; then
-    docker push item4/yui:latest
-fi
