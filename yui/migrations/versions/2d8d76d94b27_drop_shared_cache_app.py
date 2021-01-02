3b0ec4684400038065c9af3c819c8b60364459d0
Kim Jin Su <item4_hun@hotmail.com>
2021-01-02 11:05:25 +0900

    Drop shared cache model

---
+++ yui/migrations/versions/2d8d76d94b27_drop_shared_cache_app.py
@@ -0,0 +1,39 @@
+"""Drop shared cache app
+
+Revision ID: 2d8d76d94b27
+Revises: 59c36093b2ed
+Create Date: 2021-01-02 10:59:32.722200
+
+"""
+
+from alembic import op
+
+import sqlalchemy as sa
+
+from yui.orm.types import JSONType
+from yui.orm.types import TimezoneType
+
+# revision identifiers, used by Alembic.
+revision = '2d8d76d94b27'
+down_revision = '59c36093b2ed'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table('json_cache')
+
+
+def downgrade():
+    op.create_table(
+        'json_cache',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=False),
+        sa.Column('body', JSONType(), nullable=True),
+        sa.Column(
+            'created_datetime', sa.DateTime(timezone=True), nullable=False
+        ),
+        sa.Column('created_timezone', TimezoneType(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('name'),
+    )
