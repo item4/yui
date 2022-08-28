4a56f31935bfc06e0d014e41926de6fda29fe39b
Kim, Jinsu <item4hun@gmail.com>
2022-08-28 15:52:35 +0900

    Add more log for collect/cleanup

--- yui/apps/manage/cleanup/tasks.py
+++ yui/apps/manage/cleanup/tasks.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import time
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -16,31 +17,45 @@ LOGS = set[tuple[str, str]]
 
 @box.cron("0,10,20,30,40,50 * * * *")
 async def cleanup_channels(bot, sess: AsyncSession):
+    logger = logging.getLogger(
+        "yui.apps.manage.cleanup.tasks.cleanup_channels"
+    )
     try:
         channels = bot.config.CHANNELS["auto_cleanup_targets"]
     except KeyError:
+        logger.info("No auto cleanup targets. skip.")
         return
 
     naive_now = datetime.datetime.now()
     time_limit = naive_now - datetime.timedelta(hours=12)
     ts = str(time.mktime(time_limit.timetuple()))
 
+    logger.info(f"Delete messages before {ts}")
     for channel in channels:
-        await cleanup_by_event_logs(
+        logger.info(f"Start channel cleanup: {channel}")
+        deleted = await cleanup_by_event_logs(
             bot,
             sess,
             channel,
             ts,
             bot.config.OWNER_USER_TOKEN,
         )
+        logger.info(f"Finish channel cleanup: {channel}, {deleted} deleted")
 
 
 @box.cron("0 * * * *")
 async def get_old_history(bot, sess: AsyncSession):
+    logger = logging.getLogger("yui.apps.manage.cleanup.tasks.get_old_history")
     try:
         channels = bot.config.CHANNELS["auto_cleanup_targets"]
     except KeyError:
+        logger.info("No auto cleanup targets. skip.")
         return
 
     for channel in channels:
-        await collect_history_from_channel(bot, channel, sess)
+        logger.info(f"Start collect message in channel: {channel}")
+        collected = await collect_history_from_channel(bot, channel, sess)
+        logger.info(
+            f"Finish collect message in channel: {channel}"
+            f", {collected} collected"
+        )
