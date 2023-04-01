3f18da294dfe0e026c0f41434accd25f60ab9348
Kim, Jinsu <item4hun@gmail.com>
2023-04-01 15:33:39 +0900

    Remove dataclass param from yui.orm.types

--- yui/orm/types.py
+++ yui/orm/types.py
@@ -3,7 +3,5 @@ from typing import TypeAlias
 
 from sqlalchemy.orm import mapped_column
 
-PrimaryKey: TypeAlias = Annotated[
-    int, mapped_column(primary_key=True, init=False)
-]
+PrimaryKey: TypeAlias = Annotated[int, mapped_column(primary_key=True)]
 Text: TypeAlias = Annotated[str, mapped_column(deferred=True)]
