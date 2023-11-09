d8380e0a04ff462ba83fcdb662471aeff8bab745
Kim, Jinsu <item4hun@gmail.com>
2023-11-09 22:38:24 +0900

    의존성 업데이트

--- tests/apps/compute/calc_test.py
+++ tests/apps/compute/calc_test.py
@@ -513,14 +513,14 @@ def test_listcomp():
     e = Evaluator()
     assert e.run("[x ** 2 for x in [1, 2, 3]]") == [1, 4, 9]
     assert "x" not in e.symbol_table
-    assert e.run("[x ** 2 + y for x in [1, 2, 3] for y in [10, 20, 30]]") == (
-        [x**2 + y for x in [1, 2, 3] for y in [10, 20, 30]]
-    )
+    assert e.run("[x ** 2 + y for x in [1, 2, 3] for y in [10, 20, 30]]") == ([
+        x**2 + y for x in [1, 2, 3] for y in [10, 20, 30]
+    ])
     assert "x" not in e.symbol_table
     assert "y" not in e.symbol_table
-    assert e.run("[y ** 2 for x in [1, 2, 3] for y in [x+1, x+3, x+5]]") == (
-        [y**2 for x in [1, 2, 3] for y in [x + 1, x + 3, x + 5]]
-    )
+    assert e.run("[y ** 2 for x in [1, 2, 3] for y in [x+1, x+3, x+5]]") == ([
+        y**2 for x in [1, 2, 3] for y in [x + 1, x + 3, x + 5]
+    ])
     assert "x" not in e.symbol_table
     assert "y" not in e.symbol_table
 
@@ -582,14 +582,14 @@ def test_setcomp():
     e = Evaluator()
     assert e.run("{x ** 2 for x in [1, 2, 3, 3]}") == {1, 4, 9}
     assert "x" not in e.symbol_table
-    assert e.run("{x ** 2 + y for x in [1, 2, 3] for y in [10, 20, 30]}") == (
-        {x**2 + y for x in [1, 2, 3] for y in [10, 20, 30]}
-    )
+    assert e.run("{x ** 2 + y for x in [1, 2, 3] for y in [10, 20, 30]}") == ({
+        x**2 + y for x in [1, 2, 3] for y in [10, 20, 30]
+    })
     assert "x" not in e.symbol_table
     assert "y" not in e.symbol_table
-    assert e.run("{y ** 2 for x in [1, 2, 3] for y in [x+1, x+3, x+5]}") == (
-        {y**2 for x in [1, 2, 3] for y in [x + 1, x + 3, x + 5]}
-    )
+    assert e.run("{y ** 2 for x in [1, 2, 3] for y in [x+1, x+3, x+5]}") == ({
+        y**2 for x in [1, 2, 3] for y in [x + 1, x + 3, x + 5]
+    })
     assert "x" not in e.symbol_table
     assert "y" not in e.symbol_table
 
