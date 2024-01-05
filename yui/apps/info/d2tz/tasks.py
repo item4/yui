18ec16f0e69ca56e01e5a7f1aae75460781b5ff8
Kim Jinsu <item4hun@gmail.com>
2024-01-05 16:25:42 +0900

    d2tz task를 test 채널이 아닌 d2r 채널에 뿌리도록 지정

--- yui/apps/info/d2tz/tasks.py
+++ yui/apps/info/d2tz/tasks.py
@@ -4,4 +4,4 @@ from .commons import say_d2r_terror_zone_info
 
 @box.cron("1 * * * *")
 async def auto_d2tz(bot):
-    await say_d2r_terror_zone_info(bot, bot.config.CHANNELS["test"])
+    await say_d2r_terror_zone_info(bot, bot.config.CHANNELS["d2r"])
