2890acd40df6de6e3fa8a25dea2aa5c11d9fb236
Kim Jinsu <item4hun@gmail.com>
2024-03-27 12:42:24 +0900

    사전 명령어 테스트에 검색결과가 2개 이상인 경우 대응 로직 추가

--- tests/apps/search/dic_test.py
+++ tests/apps/search/dic_test.py
@@ -1,6 +1,26 @@
+import re
+
 import pytest
 
 from yui.apps.search.dic import dic
+
+MULTIPLE_PATTERN = re.compile(r"검색결과 (\d+)개의 링크를 찾았어요!")
+
+
+@pytest.mark.asyncio()
+async def test_dic_multiple(
+    bot,
+):
+    event = bot.create_message("C1", "U1", "1234.5678")
+
+    await dic(bot, event, "영어", "bad")
+
+    said = bot.call_queue.pop(0)
+    assert said.method == "chat.postMessage"
+    assert said.data["channel"] == "C1"
+    matched = MULTIPLE_PATTERN.match(said.data["text"])
+    assert matched
+    assert int(matched[1]) == len(said.data["attachments"])
 
 
 @pytest.mark.asyncio()
