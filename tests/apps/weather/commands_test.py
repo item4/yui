47353e7513cf95b0cb120c13c4fab7dcd3a29695
Kim Jinsu <item4hun@gmail.com>
2024-04-01 10:01:33 +0900

    존재하지 않는 관측소명 요청하는 경우 테스트 코드 추가

--- tests/apps/weather/commands_test.py
+++ tests/apps/weather/commands_test.py
@@ -32,3 +32,29 @@ async def test_weather_command(bot_config, cache, address):
     assert weather_said.data["text"] != "해당 주소는 찾을 수 없어요!"
     assert weather_said.data["text"] != "날씨 API 접근 중 에러가 발생했어요!"
     assert weather_said.data["text"] != "해당 이름의 관측소는 존재하지 않아요!"
+
+
+@pytest.mark.asyncio()
+async def test_weather_command_wrong_address(
+    bot_config,
+    cache,
+    unavailable_address,
+):
+    bot = FakeBot(bot_config, loop=asyncio.get_running_loop(), cache=cache)
+    bot.add_channel("C1", "general")
+    bot.add_user("U1", "item4")
+
+    event = bot.create_message("C1", "U1", "1234.5678")
+
+    async with bot.begin():
+        await weather(bot, event, unavailable_address)
+
+    weather_said = bot.call_queue.pop(0)
+
+    assert weather_said.method == "chat.postMessage"
+    assert weather_said.data["channel"] == "C1"
+
+    if weather_said.data["text"] == "날씨 API 접근 중 에러가 발생했어요!":
+        pytest.skip("Can not run test via AWS Weather API")
+
+    assert weather_said.data["text"] == "해당 이름의 관측소는 존재하지 않아요!"
