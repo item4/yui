f6c4381b7f6516dbd3b392475a25ea69c14fc26e
Kim Jinsu <item4hun@gmail.com>
2024-04-01 09:05:00 +0900

    split_chunks 함수 테스트코드 추가

--- tests/box/utils_test.py
+++ tests/box/utils_test.py
@@ -1,4 +1,5 @@
 from yui.box.utils import is_container
+from yui.box.utils import split_chunks
 
 
 def test_is_container():
@@ -11,3 +12,49 @@ def test_is_container():
     assert not is_container(int)
     assert not is_container(float)
     assert not is_container(bool)
+
+
+def test_split_chunks():
+    assert split_chunks("ls -al", use_shlex=True) == ["ls", "-al"]
+    assert split_chunks("git commit -m 'test'", use_shlex=True) == [
+        "git",
+        "commit",
+        "-m",
+        "test",
+    ]
+    assert split_chunks("test --value=b", use_shlex=True) == [
+        "test",
+        "--value=b",
+    ]
+    assert split_chunks("test --value='b c d'", use_shlex=True) == [
+        "test",
+        "--value=b c d",
+    ]
+    assert split_chunks("test\xa0--value='b c d'", use_shlex=True) == [
+        "test",
+        "--value=b c d",
+    ]
+
+    assert split_chunks("ls -al", use_shlex=False) == ["ls", "-al"]
+    assert split_chunks("git commit -m 'test'", use_shlex=False) == [
+        "git",
+        "commit",
+        "-m",
+        "'test'",
+    ]
+    assert split_chunks("test --value=b", use_shlex=False) == [
+        "test",
+        "--value=b",
+    ]
+    assert split_chunks("test --value='b c d'", use_shlex=False) == [
+        "test",
+        "--value='b",
+        "c",
+        "d'",
+    ]
+    assert split_chunks("test\xa0--value='b c d'", use_shlex=False) == [
+        "test",
+        "--value='b",
+        "c",
+        "d'",
+    ]
