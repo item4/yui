3b01003014b4866caefd178c999af888fa355c0f
Kim Jinsu <item4hun@gmail.com>
2024-04-04 11:44:55 +0900

    apps.connections.open API 호출 테스트코드 추가

---
+++ tests/api/apps_test.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.mark.asyncio()
+async def test_slack_api_apps_connections_open(bot):
+    await bot.api.apps.connections.open(token="TEST_TOKEN")  # noqa: S106
+    call = bot.call_queue.pop()
+    assert call.method == "apps.connections.open"
+    assert call.data == {}
+    assert call.token == "TEST_TOKEN"  # noqa: S105
