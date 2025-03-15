15eb61c707e66cf11ef1430669ecbfe23c949a2d
Kim Jinsu <item4hun@gmail.com>
2025-03-15 20:32:12 +0900

    RUF029 예외처리

--- yui/apps/core.py
+++ yui/apps/core.py
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 @box.on(YuiSystemStart)
-async def on_start(bot):
+async def on_start(bot):  # noqa: RUF029
     bot.is_ready.set()
     return True
 
 
 @box.on(TeamMigrationStarted)
-async def team_migration_started():
+async def team_migration_started():  # noqa: RUF029
     logger.info("Slack sent team_migration_started. restart bot")
     raise BotReconnect
