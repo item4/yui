d32555c8d0e264e04c27d8f17f5c64fe7a476085
Kim Jinsu <item4hun@gmail.com>
2025-03-23 11:39:06 +0900

    for문 관련 테스트 보강

--- tests/apps/compute/calc/evaluator/loop_test.py
+++ tests/apps/compute/calc/evaluator/loop_test.py
@@ -19,17 +19,19 @@ def test_for_continue(e):
     code = """\
 total = 0
 for x in [1, 2, 3, 4, 5, 6]:
-    total = total + x
+    total += x
     if total > 10:
         continue
-    total = total * 2
+    total *= 2
 else:
-    total = total + 10000
+    check = 100
 """
     real_locals = {}
     exec(code, locals=real_locals)  # noqa: S102 - for test only
     e.run(code)
-    assert e.scope["total"] == real_locals["total"]
+    assert e.scope["x"] == real_locals["x"] == 6
+    assert e.scope["total"] == real_locals["total"] == 26
+    assert e.scope["check"] == real_locals["check"] == 100
     assert e.current_interrupt is None
 
 
@@ -37,17 +39,64 @@ def test_for_break(e):
     code = """\
 total = 0
 for x in [1, 2, 3, 4, 5, 6]:
-    total = total + x
+    total += x
     if total > 10:
         break
-    total = total * 2
+    total *= 2
 else:
-    total = total + 10000
+    check = 100
 """
     real_locals = {}
     exec(code, locals=real_locals)  # noqa: S102 - for test only
     e.run(code)
-    assert e.scope["total"] == real_locals["total"]
+    assert e.scope["x"] == real_locals["x"] == 3
+    assert e.scope["total"] == real_locals["total"] == 11
+    assert "check" not in e.scope
+    assert e.current_interrupt is None
+
+
+def test_for_empty(e):
+    code = """\
+total = 0
+for x in []:
+    total += x
+else:
+    check = 100
+"""
+    real_locals = {}
+    exec(code, locals=real_locals)  # noqa: S102 - for test only
+    e.run(code)
+    assert "x" not in e.scope
+    assert e.scope["total"] == real_locals["total"] == 0
+    assert e.scope["check"] == real_locals["check"] == 100
+    assert e.current_interrupt is None
+
+
+def test_for_nested(e):
+    code = """\
+selected = []
+names = ["kirito", "eugeo", "asuna", "sinon"]
+for name in names:
+    for x in name:
+        if x == "n":
+            break
+    else:
+        selected.append(name)
+else:
+    check = 100
+"""
+    real_locals = {}
+    exec(code, locals=real_locals)  # noqa: S102 - for test only
+    e.run(code)
+    assert e.scope["selected"] == real_locals["selected"] == ["kirito", "eugeo"]
+    assert (
+        e.scope["names"]
+        == real_locals["names"]
+        == ["kirito", "eugeo", "asuna", "sinon"]
+    )
+    assert e.scope["name"] == real_locals["name"] == "sinon"
+    assert e.scope["x"] == real_locals["x"] == "n"
+    assert e.scope["check"] == real_locals["check"] == 100
     assert e.current_interrupt is None
 
 
