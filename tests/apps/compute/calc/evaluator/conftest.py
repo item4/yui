206dbe2b44bcbfa10768037778261fada8a1ce5a
Kim Jinsu <item4hun@gmail.com>
2025-03-23 10:45:19 +0900

    Evaluator의 decimal mode에서의 op 처리 테스트

--- tests/apps/compute/calc/evaluator/conftest.py
+++ tests/apps/compute/calc/evaluator/conftest.py
@@ -6,3 +6,8 @@ from yui.apps.compute.calc.evaluator import Evaluator
 @pytest.fixture
 def e():
     return Evaluator()
+
+
+@pytest.fixture
+def ed():
+    return Evaluator(decimal_mode=True)
--- tests/apps/compute/calc/evaluator/op_test.py
+++ tests/apps/compute/calc/evaluator/op_test.py
@@ -1,4 +1,8 @@
+import re
+
 import pytest
+
+from yui.apps.compute.calc.types import Decimal as D
 
 
 def test_binop(e):
@@ -28,3 +32,56 @@ def test_unaryop(e):
     assert e.run("not 100") == (not 100)
     assert e.run("+100") == +100
     assert e.run("-100") == -100
+
+
+def test_binop_decimal(ed):
+    error_format = (
+        "unsupported operand type(s) for {op}: 'Decimal' and 'Decimal'"
+    )
+    with pytest.raises(TypeError, match=re.escape(error_format.format(op="&"))):
+        ed.run("3 & 2")
+
+    with pytest.raises(TypeError, match=re.escape(error_format.format(op="|"))):
+        ed.run("1 | 2")
+
+    with pytest.raises(TypeError, match=re.escape(error_format.format(op="^"))):
+        assert ed.run("3 ^ 2")
+
+    with pytest.raises(
+        TypeError,
+        match=re.escape(error_format.format(op="<<")),
+    ):
+        ed.run("3 << 2")
+
+    with pytest.raises(TypeError, match=re.escape(error_format.format(op="@"))):
+        ed.run("2 @ 3")
+
+    with pytest.raises(
+        TypeError,
+        match=re.escape(error_format.format(op=">>")),
+    ):
+        ed.run("100 >> 2")
+
+    assert ed.run("1 + 2") == D(1) + D(2)
+    assert ed.run("3 / 2") == D(3) / D(2)
+    assert ed.run("3 // 2") == D(3) // D(2)
+    assert ed.run("3 * 2") == D(3) * D(2)
+    assert ed.run("33 % 4") == D(33) % D(4)
+    assert ed.run("3 ** 2") == D(3) ** D(2)
+    assert ed.run("3 - 1") == D(3) - D(1)
+
+
+def test_boolop_decimal(ed):
+    assert ed.run("True and False") is (True and False)  # noqa: SIM223
+    assert ed.run("True or False") is (True or False)  # noqa: SIM222
+
+
+def test_unaryop_decimal(ed):
+    with pytest.raises(
+        TypeError,
+        match="bad operand type for unary ~: 'Decimal'",
+    ):
+        ed.run("~100")
+    assert ed.run("not 100") == (not D(100))
+    assert ed.run("+100") == +D(100)
+    assert ed.run("-100") == -D(100)
