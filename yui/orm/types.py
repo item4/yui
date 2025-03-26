4919ade2196ccb58a7618c7bd2bd92258f68a66f
Kim Jinsu <item4hun@gmail.com>
2025-03-27 06:52:09 +0900

    typing-extensions 4.13.0으로 인해 SA mapping에 TypeAliasType을 넘기면 처리가 안 되어 우회

--- yui/orm/types.py
+++ yui/orm/types.py
@@ -2,5 +2,5 @@ from typing import Annotated
 
 from sqlalchemy.orm import mapped_column
 
-type PrimaryKey = Annotated[int, mapped_column(primary_key=True)]
-type Text = Annotated[str, mapped_column(deferred=True)]
+PrimaryKey = Annotated[int, mapped_column(primary_key=True)]
+Text = Annotated[str, mapped_column(deferred=True)]
