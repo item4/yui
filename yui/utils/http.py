ccbf131f7f8c475783630647c0cd265790675f10
Kim Jinsu <item4hun@gmail.com>
2025-06-07 15:41:37 +0900

    Fake용 User-Agent 갱신

--- yui/utils/http.py
+++ yui/utils/http.py
@@ -1,6 +1,6 @@
 from typing import Final
 
 # NOTE: https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
-USER_AGENT: Final[str] = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+USER_AGENT: Final = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 )
